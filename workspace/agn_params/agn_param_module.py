b3774bbe14d4a521e9173024bf6a7ff072577447
danielsf <scott.f.daniel@gmail.com>
2018-01-19 13:12:39 -0800

    cache d_wavelen

--- workspace/agn_params/agn_param_module.py
+++ workspace/agn_params/agn_param_module.py
@@ -133,13 +133,20 @@ def k_correction(sed_obj, bp, redshift):
 
     if not hasattr(k_correction, '_valid_dex_dict'):
         k_correction._valid_dex_dict = {}
+        k_correction._d_wavelen_dict = {}
 
     if bp not in k_correction._valid_dex_dict:
         print('calculating valid dexes')
         valid_bp_dex = np.where(np.abs(bp.sb)>0.0)
         k_correction._valid_dex_dict[bp] = valid_bp_dex
+
+        d_wavelen = bp.wavelen[1:]-bp.wavelen[:-1]
+        k_correction._d_wavelen_dict[bp] = d_wavelen
+
+
     else:
         valid_bp_dex = k_correction._valid_dex_dict[bp]
+        d_wavelen = k_correction._d_wavelen_dict[bp]
 
     restframe_min_wavelen = restframe_wavelen_grid[valid_bp_dex[0][0]]
     restframe_max_wavelen = restframe_wavelen_grid[valid_bp_dex[0][-1]]
@@ -156,7 +163,6 @@ def k_correction(sed_obj, bp, redshift):
 
         raise RuntimeError(msg)
 
-
     restframe_fnu = np.interp(restframe_wavelen_grid,
                               sed_obj.wavelen,
                               sed_obj.fnu,
@@ -171,11 +177,11 @@ def k_correction(sed_obj, bp, redshift):
 
     restframe_integral = (0.5*(bp.sb[1:]*restframe_fnu[1:]/bp.wavelen[1:] +
                                bp.sb[:-1]*restframe_fnu[:-1]/bp.wavelen[:-1]) *
-                              (bp.wavelen[1:]-bp.wavelen[:-1])).sum()
+                              d_wavelen).sum()
 
     observer_integral = (0.5*(bp.sb[1:]*observed_fnu[1:]/bp.wavelen[1:] +
                               bp.sb[:-1]*observed_fnu[:-1]/bp.wavelen[:-1]) *
-                             (bp.wavelen[1:]-bp.wavelen[:-1])).sum()
+                             d_wavelen).sum()
 
     return -2.5*np.log10((1.0+redshift)*observer_integral/restframe_integral)
 
