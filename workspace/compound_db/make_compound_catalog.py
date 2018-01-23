3cbbebfcfc63a1427b3e0559b1cd674cca58d94d
danielsf <scott.f.daniel@gmail.com>
2018-01-22 22:45:16 -0800

    naively convert example to DESCQA (fails)

--- workspace/compound_db/make_compound_catalog.py
+++ workspace/compound_db/make_compound_catalog.py
@@ -1,21 +1,8 @@
-from lsst.sims.catUtils.baseCatalogModels import GalaxyBulgeObj
-from lsst.sims.catUtils.baseCatalogModels import GalaxyDiskObj
-from lsst.sims.catUtils.baseCatalogModels import GalaxyTileCompoundObj
 from lsst.sims.catalogs.definitions import CompoundInstanceCatalog
+from desc.sims.GCRCatSimInterface import bulgeDESCQAObject
+from desc.sims.GCRCatSimInterface import diskDESCQAObject
 from lsst.sims.catalogs.definitions import InstanceCatalog
 from lsst.sims.utils import ObservationMetaData
-
-class _fatboy(object):
-    database = 'LSSTCATSIM'
-    host = 'fatboy.phys.washington.edu'
-    port = 1433
-    driver = 'mssql+pymssql'
-
-class GalaxyBulgeObj_fatboy(_fatboy, GalaxyBulgeObj):
-    pass
-
-class GalaxyDiskObj_fatboy(_fatboy, GalaxyDiskObj):
-    pass
 
 class CatForBulge(InstanceCatalog):
     cannot_be_null = ['sedFilename']
@@ -33,9 +20,8 @@ if __name__ == "__main__":
                               boundType='circle', boundLength=0.01)
 
     cat = CompoundInstanceCatalog([CatForBulge, CatForDisk],
-                                  [GalaxyBulgeObj_fatboy,
-                                   GalaxyDiskObj_fatboy],
-                                  obs_metadata=obs,
-                                  compoundDBclass=GalaxyTileCompoundObj)
+                                  [bulgeDESCQAObject,
+                                   diskDESCQAObject],
+                                  obs_metadata=obs)
 
-    cat.write_catalog('compound_cat.txt', chunk_size=10000)
+    cat.write_catalog('descqa_compound_cat.txt', chunk_size=10000)
