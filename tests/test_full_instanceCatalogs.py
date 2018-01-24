2f313536064850ad9cc84254c0c4432a5c760c32
danielsf <scott.f.daniel@gmail.com>
2018-01-24 10:45:41 -0800

    unit test uses tempfile to make test directory

--- tests/test_full_instanceCatalogs.py
+++ tests/test_full_instanceCatalogs.py
@@ -1,5 +1,7 @@
 import unittest
 import os
+import tempfile
+import shutil
 
 from lsst.utils import getPackageDir
 from desc.sims.GCRCatSimInterface import diskDESCQAObject_protoDC2
@@ -14,12 +16,16 @@ mag_grid = os.path.join(getPackageDir('sims_GCRCatSimInterface'), 'data',
                  'Have not created SED magnitude grid, yet')
 class BulgePhoSimCatalogTestCase(unittest.TestCase):
 
-    def setUp(self):
-        self.cat_name = 'dummy_disk_catalog.txt'
+    @classmethod
+    def setUpClass(cls):
+        cls.out_dir = tempfile.mkdtemp(prefix='full_instanceCatalog')
 
-    def tearDown(self):
-        if os.path.exists(self.cat_name):
-            os.unlink(self.cat_name)
+    @classmethod
+    def tearDown(cls):
+        list_of_files = os.listdir(cls.out_dir)
+        for file_name in list_of_files:
+            os.unlink(os.path.join(cls.out_dir, file_name))
+        shutil.rmtree(cls.out_dir)
 
     def test_dummy_disk_catalog(self):
         """
@@ -36,11 +42,16 @@ class BulgePhoSimCatalogTestCase(unittest.TestCase):
                                   boundType='circle', boundLength=0.01)
         cat = PhoSimDESCQA(db, obs_metadata=obs, cannot_be_null=['hasDisk'])
         cat.phoSimHeaderMap = {}
-        cat.write_catalog(self.cat_name)
-        with open(self.cat_name, 'r') as in_file:
+        cat_name = os.path.join(self.out_dir, 'disk_phosim_cat.txt')
+        cat.write_catalog(cat_name)
+        with open(cat_name, 'r') as in_file:
             cat_lines = in_file.readlines()
 
         self.assertGreater(len(cat_lines), 100)
+
+        if os.path.exists(cat_name):
+            os.unlink(cat_name)
+
 
 if __name__ == "__main__":
     unittest.main()
