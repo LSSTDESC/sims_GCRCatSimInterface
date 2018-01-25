79ad463f9b6b7fca013526ac3e74d2541bf29cac
danielsf <scott.f.daniel@gmail.com>
2018-01-25 10:44:41 -0800

    add objid to agnDESCQAObject_protoDC2

--- python/desc/sims/GCRCatSimInterface/ProtoDC2DatabaseEmulator.py
+++ python/desc/sims/GCRCatSimInterface/ProtoDC2DatabaseEmulator.py
@@ -266,6 +266,7 @@ class knotsDESCQAObject_protoDC2(DESCQAObject_protoDC2):
 
 class agnDESCQAObject_protoDC2(DESCQAObject_protoDC2):
     objectTypeId = 117
+    objid = 'agn_descqa'
     _columns_need_postfix = False
 
     descqaDefaultValues = {'varParamStr': (None, (str, 500)),
