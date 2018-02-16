c0ff8e8ea4cb6b2567e1fa5e5a974c5cea582f9c
danielsf <scott.f.daniel@gmail.com>
2018-02-16 10:38:11 -0800

    clean up whitespace

--- python/desc/sims/GCRCatSimInterface/CatalogClasses.py
+++ python/desc/sims/GCRCatSimInterface/CatalogClasses.py
@@ -28,7 +28,7 @@ class DC2PhosimCatalogSN(PhoSimCatalogSN):
     def get_shorterFileNames(self):
         """
         Method to truncate filenames for transient
-        spectra written out by phosim. 
+        spectra written out by phosim.
 
         .. note: the variable sep needs to be in
         `self.sn_sedfile_prefix` before writing out
@@ -39,7 +39,7 @@ class DC2PhosimCatalogSN(PhoSimCatalogSN):
         split_names = []
         for fname in fnames:
             if 'None' not in fname:
-                fname = sep + fname.split(sep)[-1] 
+                fname = sep + fname.split(sep)[-1]
             else:
                 fname = 'None'
             split_names.append(fname)
@@ -55,7 +55,7 @@ class DC2PhosimCatalogSN(PhoSimCatalogSN):
                       'galacticExtinctionModel', 'galacticAv', 'galacticRv']
 
     cannot_be_null = ['x0', 't0', 'z', 'shorterFileNames']
-    
+
     default_columns = [('gamma1', 0., float), ('gamma2', 0., float), ('kappa', 0., float),
                        ('raOffset', 0., float), ('decOffset', 0., float),
                        ('galacticAv', 0.1, float), ('galacticRv', 3.1, float),
