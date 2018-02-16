674f312658bf97e84abadcd199af69e1368848c3
danielsf <scott.f.daniel@gmail.com>
2018-02-16 10:19:14 -0800

    add all columns that are needed

--- bin.src/generate_dc2_reference_catalog.py
+++ bin.src/generate_dc2_reference_catalog.py
@@ -5,19 +5,40 @@ from lsst.sims.catalogs.definitions import InstanceCatalog
 from lsst.sims.catUtils.mixins import AstrometryStars, PhotometryStars
 from lsst.sims.utils import ObservationMetaData
 from lsst.sims.catUtils.baseCatalogModels import StarObj
+from lsst.sims.utils import arcsecFromRadians
 
 import copy
 import argparse
 
 
-class Dc2RefCat(InstanceCatalog, AstrometryStars, PhotometryStars):
+class Dc2RefCatMixin(InstanceCatalog):
     column_outputs = ['uniqueId', 'raJ2000', 'decJ2000',
-                      'lsst_u', 'lsst_g', 'lsst_r', 'lsst_i', 'lsst_z',
-                      'lsst_y', 'isresolved', 'isvariable']
-    default_columns = [('isresolved', 0, int), ('isvariable', 0, int)]
-    transformations = {'raJ2000': np.degrees, 'decJ2000': np.degrees}
-    default_formats = {'S': '%s', 'f': '%.8f', 'i': '%i'}
+                      'lsst_u', 'sigma_lsst_u',
+                      'lsst_g', 'sigma_lsst_g',
+                      'lsst_r', 'sigma_lsst_r',
+                      'lsst_i', 'sigma_lsst_i',
+                      'lsst_z', 'sigma_lsst_z',
+                      'lsst_y', 'sigma_lsst_y',
+                      'isresolved', 'isvariable',
+                      'properMotionRa', 'properMotionDec', 'parallax',
+                      'radialVelocity']
 
+    default_columns = [('isresolved', 0, int), ('isvariable', 0, int)]
+
+    transformations = {'raJ2000': np.degrees, 'decJ2000': np.degrees,
+                       'properMotionDec': arcsecFromRadians,
+                       'properMotionRa': arcsecFromRadians,
+                       'parallax': arcsecFromRadians}
+
+    default_formats = {'S': '%s', 'f': '%.8f', 'i': '%i'}
+    override_formats = {'properMotionRa': '%.8g',
+                        'properMotionDec': '%.8g',
+                        'parallax': '%.8g'}
+
+
+
+class Dc2RefCatStars(AstrometryStars, PhotometryStars, Dc2RefCatMixin):
+    pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -44,12 +65,14 @@ if __name__ == "__main__":
     obs = ObservationMetaData(pointingRA=args.ra,
                               pointingDec=args.dec,
                               boundType='circle',
-                              boundLength=args.fov)
+                              boundLength=args.fov,
+                              bandpassName = ['u','g','r','i','z','y'])
 
+    obs.m5 = [23.68, 24.89, 24.43, 24.0, 24.45, 22.60]
 
     star_db = StarObj(database='LSSTCATSIM', host='fatboy.phys.washington.edu',
                       port=1433, driver='mssql+pymssql')
 
-    cat = Dc2RefCat(star_db, obs_metadata=obs)
+    cat = Dc2RefCatStars(star_db, obs_metadata=obs)
     file_name = os.path.join(args.out_dir, 'dc2_reference_catalog.txt')
     cat.write_catalog(file_name, chunk_size=10000)
