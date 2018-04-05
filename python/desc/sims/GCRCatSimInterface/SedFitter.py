32e3128691676e11fb4603478689a79f57243a62
danielsf <scott.f.daniel@gmail.com>
2018-04-05 10:31:10 -0700

    factor out filter name acquisition into own method

--- python/desc/sims/GCRCatSimInterface/SedFitter.py
+++ python/desc/sims/GCRCatSimInterface/SedFitter.py
@@ -5,10 +5,82 @@ import GCRCatalogs
 from lsst.utils import getPackageDir
 from lsst.sims.photUtils import BandpassDict, Bandpass, Sed, CosmologyObject
 
-__all__ = ["sed_from_galacticus_mags"]
+__all__ = ["sed_filter_names_from_catalog", "sed_from_galacticus_mags"]
 
-_gcr_sed_re = re.compile(r'sed_(\d+)_(\d+)_(bulge|disk)$')
 _galaxy_sed_dir = os.path.join(getPackageDir('sims_sed_library'), 'galaxySED')
+
+
+def sed_filter_names_from_catalog(catalog):
+    """
+    Takes an already-loaded GCR catalog and returns the names, wavelengths,
+    and widths of the SED-defining bandpasses
+
+    Parameters
+    ----------
+    catalog -- is a catalog loaded with GCR.load_catalog()
+
+    Returns
+    -------
+    A dict keyed to 'bulge' and 'disk'.  The values in this dict will
+    be dicts keyed to 'filter_name', 'wav_min', 'wav_width'.  The
+    corresponding values are:
+
+    filter_name -- list of the names of the columns defining the SED
+    wav_min -- list of the minimum wavelengths of SED-defining bandpasses (in nm)
+    wav_width -- list of the widths of the SED-defining bandpasses (in nm)
+
+    All outputs will be returned in order of increasing wav_min
+    """
+
+    disk_re = re.compile(r'sed_(\d+)_(\d+)_disk$')
+    bulge_re = re.compile(r'sed_(\d+)_(\d+)_bulge$')
+
+    all_quantities = catalog.list_all_quantities()
+
+    bulge_names = []
+    bulge_wav_min = []
+    bulge_wav_width = []
+
+    disk_names = []
+    disk_wav_min = []
+    disk_wav_width = []
+
+    for qty_name in all_quantities:
+        disk_match = disk_re.match(qty_name)
+        if disk_match is not None:
+            disk_names.append(qty_name)
+            disk_wav_min.append(0.1*float(disk_match[1]))  # 0.1 converts to nm
+            disk_wav_width.append(0.1*float(disk_match[2]))
+
+        bulge_match = bulge_re.match(qty_name)
+        if bulge_match is not None:
+            bulge_names.append(qty_name)
+            bulge_wav_min.append(0.1*float(bulge_match[1]))
+            bulge_wav_width.append(0.1*float(bulge_match[2]))
+
+    disk_wav_min = np.array(disk_wav_min)
+    disk_wav_width = np.array(disk_wav_width)
+    disk_names = np.array(disk_names)
+    sorted_dex = np.argsort(disk_wav_min)
+    disk_wav_width = disk_wav_width[sorted_dex]
+    disk_names = disk_names[sorted_dex]
+    disk_wav_min = disk_wav_min[sorted_dex]
+
+    bulge_wav_min = np.array(bulge_wav_min)
+    bulge_wav_width = np.array(bulge_wav_width)
+    bulge_names = np.array(bulge_names)
+    sorted_dex = np.argsort(bulge_wav_min)
+    bulge_wav_width = bulge_wav_width[sorted_dex]
+    bulge_names = bulge_names[sorted_dex]
+    bulge_wav_min = bulge_wav_min[sorted_dex]
+
+    return {'disk':{'filter_name': disk_names,
+                    'wav_min': disk_wav_min,
+                    'wav_width': disk_wav_width},
+            'bulge':{'filter_name': bulge_names,
+                     'wav_min': bulge_wav_min,
+                     'wav_width': bulge_wav_width}}
+
 
 def _get_sed_mags_and_cosmology(catalog_name):
     """
@@ -26,33 +98,50 @@ def _get_sed_mags_and_cosmology(catalog_name):
     gc = GCRCatalogs.load_catalog(catalog_name, config_overwrite=dict(md5=False))
     cosmo = dict(H0=gc.cosmology.H0.value, Om0=gc.cosmology.Om0)
 
-    bp_params_raw = {'disk': set(), 'bulge': set()}
-    for q in gc.list_all_quantities():
-        m = _gcr_sed_re.match(q)
-        if m:
-            wav0, width, tag = m.groups()
-            bp_params_raw[tag].add((int(wav0), int(width)))
-    assert bp_params_raw['disk'] == bp_params_raw['bulge'], 'SEDs for disk and bulge do not match'
-    assert bp_params_raw['disk'], 'No SED found'
+    sed_filter_dict = sed_filter_names_from_catalog(gc)
 
-    bp_params_sorted = sorted(bp_params_raw['disk'], key=lambda p: p[0])
+    disk_names = sed_filter_dict['disk']['filter_name']
+    bulge_names = sed_filter_dict['bulge']['filter_name']
 
-    # SED labels in GCR specify the band pass in angstrom, but CatSim uses nm
-    # Hence the conversion factor 0.1 in the code below
-    wav_min = bp_params_sorted[0][0] * 0.1
-    wav_max = max((0.1*(wav0+width) for wav0, width in bp_params_sorted))
-    wav_grid = np.arange(wav_min, wav_max, 0.1)
+    try:
+        for d_name in disk_names:
+            b_name = d_name.replace('disk', 'bulge')
+            assert b_name in bulge_names
+        assert len(disk_names) == len(bulge_names)
+    except AssertionError:
+        for ii in range(max(len(bulge_names), len(disk_names))):
+            if ii < len(disk_names):
+                d_name = disk_names[ii]
+            else:
+                d_name = None
 
-    wav_min_grid = np.zeros(len(bp_params_sorted))
-    wav_width_grid = np.zeros(len(bp_params_sorted))
-    for i_bp, bp in enumerate(bp_params_sorted):
-        wav_min_grid[i_bp] = bp[0]*0.1  # 0.1 factor converts to nm
-        wav_width_grid[i_bp] = bp[1]*0.1
+            if ii < len(bulge_names):
+                b_name = bulge_names[ii]
+            else:
+                b_name = None
+            print('%s %s' % (b_name, d_name))
+
+        raise RuntimeError("disk and bulge SED filter names do not "
+                           "match in catalog")
+
+    np.testing.assert_array_almost_equal(sed_filter_dict['disk']['wav_min'],
+                                         sed_filter_dict['bulge']['wav_min'],
+                                         decimal=10)
+
+    np.testing.assert_array_almost_equal(sed_filter_dict['disk']['wav_width'],
+                                         sed_filter_dict['bulge']['wav_width'],
+                                         decimal=10)
+
+    wav_min_grid = sed_filter_dict['disk']['wav_min']
+    wav_width_grid = sed_filter_dict['disk']['wav_width']
+    wav_max = max((wav0+width
+                  for wav0, width in zip(wav_min_grid, wav_width_grid)))
+    wav_grid = np.arange(wav_min_grid.min(), wav_max, 0.1)
 
     bp_name_list = list()
     bp_list = list()
-    for wav0, width in bp_params_sorted:
-        sb_grid = ((wav_grid >= wav0*0.1) & (wav_grid <= (wav0+width)*0.1)).astype(float)
+    for wav0, width in zip(wav_min_grid, wav_width_grid):
+        sb_grid = ((wav_grid >= wav0) & (wav_grid <= (wav0+width))).astype(float)
         bp_list.append(Bandpass(wavelen=wav_grid, sb=sb_grid))
         bp_name_list.append('%d_%d' % (wav0, width))
 
