29efe7d786a141837de8555513f091c87659527e
danielsf <scott.f.daniel@gmail.com>
2018-06-29 17:02:24 -0700

    add script to run truth writer

---
+++ test_truth_params.py
@@ -0,0 +1,15 @@
+import os
+from desc.sims.GCRCatSimInterface import write_sprinkled_truth
+from lsst.sims.utils import ObservationMetaData
+
+agn_dir = '/global/projecta/projectdirs/lsst/groups/SSim/DC2'
+assert os.path.isdir(agn_dir)
+
+agn_db = os.path.join(agn_dir, 'agn_db_mbh_7.0_m_i_30.0.sqlite')
+assert os.path.isfile(agn_db)
+
+obs = ObservationMetaData(pointingRA=53.13231600394978216,
+                          pointingDec=-28.03064232384672749,
+                          mjd=62746.27986361111106817)
+
+write_sprinkled_truth(obs, agn_db=agn_db)
