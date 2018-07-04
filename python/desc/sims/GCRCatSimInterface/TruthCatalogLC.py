5df201e9d9e8e2266d67d7456be2427ec2198e13
danielsf <scott.f.daniel@gmail.com>
2018-07-04 10:33:53 -0700

    write light curves to sqlite file, rather hdf5

--- python/desc/sims/GCRCatSimInterface/TruthCatalogLC.py
+++ python/desc/sims/GCRCatSimInterface/TruthCatalogLC.py
@@ -1,8 +1,8 @@
-import h5py
 import tempfile
 import os
 import numpy as np
 import shutil
+import sqlite3
 import time
 from lsst.sims.catalogs.db import DBObject
 from lsst.sims.catUtils.mixins import ExtraGalacticVariabilityModels
@@ -24,12 +24,25 @@ class AgnSimulator(ExtraGalacticVariabilityModels, TimeDelayVariability):
         return self._redshift_arr
 
 
-def write_sprinkled_lc(h5_file, total_obs_md,
+def create_sql_file(sql_name):
+    with sqlite3.connect(sql_name) as conn:
+        cursor = conn.cursor()
+        cmd = '''CREATE TABLE agn '''
+        cmd += '''(uniqueId int,
+                   obshistid int, u float, g float,
+                   r float, i float, z float, y float)'''
+
+        cursor.execute(cmd)
+        conn.commit()
+
+def write_sprinkled_lc(out_file_name, total_obs_md,
                        pointing_dir, opsim_db_name,
                        field_ra=55.064, field_dec=-29.783,
                        agn_db=None, yaml_file='proto-dc2_v4.6.1',
                        ra_colname='descDitheredRA',
                        dec_colname='descDitheredDec'):
+
+    create_sql_file(out_file_name)
 
     (htmid_dict,
      mjd_dict) = get_pointing_htmid(pointing_dir, opsim_db_name,
@@ -72,10 +85,11 @@ def write_sprinkled_lc(h5_file, total_obs_md,
     agn_base_query += 'FROM zpoint '
 
     n_floats = 0
-    with h5py.File(h5_file, 'w') as out_file:
-        sprinkled_group = out_file.create_group('sprinkled')
+    with sqlite3.connect(out_file_name) as conn:
+        cursor = conn.cursor()
         for htmid_dex, htmid in enumerate(object_htmid):
             mjd_arr = []
+            obs_arr = []
             t_start = time.time()
             for obshistid in htmid_dict:
                 is_contained = False
@@ -85,14 +99,16 @@ def write_sprinkled_lc(h5_file, total_obs_md,
                         break
                 if is_contained:
                     mjd_arr.append(mjd_dict[obshistid])
-            mjd_arr = np.sort(np.array(mjd_arr))
-            duration = time.time()-t_start
-            print('made mjd_arr in %e seconds' % duration)
+                    obs_arr.append(obshistid)
             if len(mjd_arr) == 0:
                 continue
-            htm_group = sprinkled_group.create_group('%d' % htmid)
-            htm_group.attrs.create('htmid', htmid, dtype=np.int)
-            htm_group.attrs.create('mjd', mjd_arr)
+            mjd_arr = np.array(mjd_arr)
+            obs_arr = np.array(obs_arr)
+            sorted_dex = np.argsort(mjd_arr)
+            mjd_arr = mjd_arr[sorted_dex]
+            obs_arr = obs_arr[sorted_dex]
+            duration = time.time()-t_start
+            print('made mjd_arr in %e seconds' % duration)
 
             agn_query = agn_base_query + 'WHERE htmid=%d and is_agn=1' % htmid
 
@@ -106,10 +122,21 @@ def write_sprinkled_lc(h5_file, total_obs_md,
                 dmag = agn_simulator.applyVariability(agn_results['varParamStr'],
                                                       expmjd=mjd_arr)
 
-                dmag_name = 'agn_dmag_%d' % (i_chunk)
-                htm_group.create_dataset(dmag_name,
-                                         data=dmag.transpose(1,0,2))
 
+                for i_time in range(len(obs_arr)):
+                    values = ((int(agn_results['uniqueId'][i_obj]),
+                               int(obs_arr[i_time]),
+                               dmag[0][i_obj][i_time],
+                               dmag[1][i_obj][i_time],
+                               dmag[2][i_obj][i_time],
+                               dmag[3][i_obj][i_time],
+                               dmag[4][i_obj][i_time],
+                               dmag[5][i_obj][i_time])
+                              for i_obj in range(len(agn_results)))
+                    cursor.executemany('''INSERT INTO agn VALUES
+                                       (?,?,?,?,?,?,?,?)''', values)
+
+                conn.commit()
                 n_floats += len(dmag.flatten())
 
     print('n_floats %d' % n_floats)
