758bdd391ea9291c5c80a169b9c1738197d77a9d
danielsf <scott.f.daniel@gmail.com>
2018-07-12 13:58:54 -0700

    fix docstring

--- python/desc/sims/GCRCatSimInterface/TruthCatalogUtils.py
+++ python/desc/sims/GCRCatSimInterface/TruthCatalogUtils.py
@@ -145,11 +145,8 @@ def write_sprinkled_param_db(obs, field_ra=55.064, field_dec=-29.783,
 
     Returns
     -------
-    None
-
-    Writes a sqlite database out_dir/sprinkled_objects.sqlite containing
-    the parameters needed to calculate truth information for all
-    extragalactic sources.
+    The name of the written sqlite database and a list of the tables
+    it contains.
     """
     if not os.path.isfile(agn_db):
         raise RuntimeError("%s is not a valid file" % agn_db)
