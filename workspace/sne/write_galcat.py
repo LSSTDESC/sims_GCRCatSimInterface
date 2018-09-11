92a6ebb99443126ed463f5faac88cfe4c95f6816
danielsf <scott.f.daniel@gmail.com>
2018-09-11 10:21:27 -0700

    write_galcat.py loops over all healpixels

--- workspace/sne/write_galcat.py
+++ workspace/sne/write_galcat.py
@@ -11,7 +11,7 @@ for key in GCRCatalogs.available_catalogs:
 import pandas as pd
 
 
-def write_hdf(healpixel):
+def write_hdf(healpixel, out_dir):
     gc = GCRCatalogs.load_catalog('cosmoDC2_v1.0_image',
                               {'healpix_pixels': [healpixel]})
 
@@ -19,6 +19,13 @@ def write_hdf(healpixel):
                        'redshift_true', 'size_disk_true', 'size_minor_disk_true', 'size_minor_bulge_true',
                        'size_bulge_true', 'ra', 'dec', 'position_angle_true'], return_iterator=True)
     df = pd.DataFrame(next(x))
-    df.to_hdf(os.path.join(os.environ['SCRATCH'],'gals_ra_dec.hdf'),index=False, key='0')
+    df.to_hdf(os.path.join(out_dir,'gals_%d_ra_dec.hdf' % healpixel),index=False, key='0')
 
-write_hdf(8786)
+
+if __name__ == "__main__":
+    cat_config = GCRCatalogs.get_catalog_config('cosmoDC2_v1.0_image')
+    healpix_list = cat_config['healpix_pixels']
+    out_dir = os.path.join(os.environ['SCRATCH'], 'sne_production')
+    assert os.path.isdir(out_dir)
+    for hh in healpix_list:
+        write_hdf(hh, out_dir)
