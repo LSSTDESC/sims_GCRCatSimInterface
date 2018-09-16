f41e7c33db340cce68b525019a6e811418f71de5
EiffL <fr.eiffel@gmail.com>
2018-09-16 16:26:33 -0700

    Replace dropping by clipping

--- bin.src/instance_crawler.py
+++ bin.src/instance_crawler.py
@@ -48,43 +48,31 @@ def fopen_generator(fd, abspath, **kwds):
                     for line in my_input:
                         yield line
 
-def main(in_instcat_disk, in_instcat_bulge, in_instcat_knots,
-         out_instcat_disk, out_instcat_bulge, out_instcat_knots):
+def fix_disk_knots(in_instcat_disk, in_instcat_knots,
+         out_instcat_disk, out_instcat_knots):
 
     # Use .fopen to read in the command and object lines from the
     # instance catalog.
-    count = 0
-    skipped = 0
+    count_knots = 0
+    count_extinction = 0
     with fopen(in_instcat_disk, mode='rt') as input_disk,   \
-         fopen(in_instcat_bulge, mode='rt') as input_bulge,  \
          fopen(in_instcat_knots, mode='rt') as input_knots,  \
          open(out_instcat_disk, 'w') as output_disk, \
-         open(out_instcat_bulge, 'w') as output_bulge, \
          open(out_instcat_knots, 'w') as output_knots:
 
         # We first go through the knots catalog because some entries are missing
         # compared to the full bulge/disk catalog (faint knots have already been
         # removed at the instance catalog creation level)
         for line_knots in input_knots:
-
             # Extract the galaxy ID for that knots component
             tokens_knots = line_knots.strip().split()
             id_knots = int(tokens_knots[1]) >> 10
 
             found = False
-            offensive = False
-            # Loop through the disk and bulge catalogs
-            for line_disk, line_bulge in zip(input_disk, input_bulge):
+            # Loop through the disk catalogs
+            for line_disk in input_disk:
                 tokens_disk = line_disk.strip().split()
                 id_disk = int(tokens_disk[1]) >> 10
-
-                tokens_bulge = line_bulge.strip().split()
-                id_bulge = int(tokens_bulge[1]) >> 10
-
-                # Checking that both components are for the same object
-                if id_disk != id_bulge:
-                    print("ERROR: Mismatch between disk and bulge catalogs", id_disk, id_bulge)
-                    exit(-1)
 
                 # Extracting the extinction parameters:
                 if tokens_disk[17].lower() != 'none':
@@ -94,40 +82,24 @@ def main(in_instcat_disk, in_instcat_bulge, in_instcat_knots,
                     disk_internal_av = 0
                     disk_internal_rv = 0
 
-                if tokens_bulge[17].lower() != 'none':
-                    bulge_internal_av = float(tokens_bulge[18])
-                    bulge_internal_rv = float(tokens_bulge[19])
-                else:
-                    bulge_internal_av = 0
-                    bulge_internal_rv = 0
-
-
-                # Applying check on extinction parameters
-                if ((disk_internal_av < 0) or (bulge_internal_av < 0) or
-                    (disk_internal_rv > 1) or (bulge_internal_rv > 1)):
-
-                    # Detected offensive object that shall be unceremoniously dropped
-                    print('Dropping offensive galaxy %d with disk/bulge av/rv %f/%f %f/%f'%(id_disk,
-                            disk_internal_av,disk_internal_rv,bulge_internal_av,bulge_internal_rv))
-                    offensive = True
-                    skipped +=1
-                    # We are not printing these entries in the output instance catalog,
-                    # and will also skip that knots entry
-                    break
+                # If the galaxy is offensive, clip the av and rv values
+                if disk_internal_av < 0 or disk_internal_rv > 1:
+                    print('Fixing offensive disk %d with av/rv extinction: %f/%f'%(id_disk,disk_internal_av, disk_internal_rv))
+                    disk_internal_av = np.clip(disk_internal_av,0.0,None)
+                    disk_internal_rv = np.clip(disk_internal_rv,None,1.0)
+                    tokens_disk[18] = ("%.7f"%disk_internal_av).rstrip('0')
+                    tokens_disk[19] = ("%.7f"%disk_internal_rv).rstrip('0')
+                    count_extinction += 1
 
                 if id_disk == id_knots:
                     found=True
                     break
                 else:
+                    line_disk = ' '.join(tokens_disk)
                     output_disk.write(line_disk.strip()+'\n')
-                    output_bulge.write(line_bulge.strip()+'\n')
-
-            if offensive:
-                # Ignoring that knots entry because it was found to be offensive
-                continue
 
             if not found:
-                print("ERROR: object ids do not match between input catalogs")
+                print("ERROR: object ids do not match between input knots and disks catalogs")
                 exit(-1)
 
             # Get total flux
@@ -138,13 +110,9 @@ def main(in_instcat_disk, in_instcat_bulge, in_instcat_knots,
 
             # Apply flux cap for large galaxies
             size = np.float(tokens_disk[13])
-            if size > 2.5:
-                knots_flux_ratio = np.clip(knots_flux_ratio, 0, 0.3)
-                count+=1
-                print("Capping knots flux for object %d, with magnorm: %f and size %f"%(id_knots,magnorm_disk,size))
-            elif size > 1.:
+            if size > 1.:
                 knots_flux_ratio = np.clip(knots_flux_ratio, 0, 0.5)
-                count+=1
+                count_knots+=1
                 print("Capping knots flux for object %d, with magnorm: %f and size %f"%(id_knots,magnorm_disk,size))
 
             magnorm_disk = -2.5*np.log10((1-knots_flux_ratio)*total_flux)
@@ -153,16 +121,49 @@ def main(in_instcat_disk, in_instcat_bulge, in_instcat_knots,
             # Update the entry
             tokens_disk[4] = ("%.7f"%magnorm_disk).rstrip('0')
             tokens_knots[4] = ("%.7f"%magnorm_knots).rstrip('0')
+            # Making sure that the extinction paramters remain the same between disk and knots
+            tokens_knots[18] = tokens_disk[18]
+            tokens_knots[19] = tokens_disk[19]
             line_disk = ' '.join(tokens_disk)
             line_knots = ' '.join(tokens_knots)
 
-            # Write
+            # Write the catalogs
             output_disk.write(line_disk.strip()+'\n')
             output_knots.write(line_knots.strip()+'\n')
-            output_bulge.write(line_bulge.strip()+'\n')
 
-    print("Fixed %d galaxies"%count)
-    print("Skipped %d galaxies"%skipped)
+    print("Corrected %d knots"%count_knots)
+    print("Fixed extinction for %d disks"%count_extinction)
+
+
+def fix_bulge(in_instcat_bulge, out_instcat_bulge):
+    count_extinction = 0
+    with fopen(in_instcat_bulge, mode='rt') as input_bulge,  \
+         open(out_instcat_bulge, 'w') as output_bulge:
+
+        # Now we go through the bulge catalog independently, fixing the extinction
+        for line_bulge in input_bulge:
+            tokens_bulge = line_bulge.strip().split()
+            id_bulge = int(tokens_bulge[1]) >> 10
+
+            # Extracting the extinction parameters:
+            if tokens_bulge[17].lower() != 'none':
+                internal_av = float(tokens_bulge[18])
+                internal_rv = float(tokens_bulge[19])
+            else:
+                internal_av = 0
+                internal_rv = 0
+
+            # If the galaxy is offensive, clip the av and rv values
+            if internal_av < 0 or internal_rv > 1:
+                print('Fixing offensive bulge %d with av/rv extinction: %f/%f'%(id_bulge,internal_av, internal_rv))
+                internal_av = np.clip(internal_av,0.0,None)
+                internal_rv = np.clip(internal_rv,None,1.0)
+                tokens_bulge[18] = ("%.7f"%internal_av).rstrip('0')
+                tokens_bulge[19] = ("%.7f"%internal_rv).rstrip('0')
+
+            line_bulge = ' '.join(tokens_bulge)
+            output_bulge.write(line_bulge.strip()+'\n')
+    print("Fixed extinction for %d bulge"%count_extinction)
 
 if __name__ == '__main__':
 
@@ -174,5 +175,5 @@ if __name__ == '__main__':
     parser.add_argument('output_bulge', type=str)
     parser.add_argument('output_knots', type=str)
     args = parser.parse_args()
-    main(args.input_disk, args.input_bulge, args.input_knots,
-         args.output_disk,args.output_bulge, args.output_knots)
+    fix_disk_knots(args.input_disk, args.input_knots, args.output_disk, args.output_knots)
+    fix_bulge(args.input_bulge, args.output_bulge)
