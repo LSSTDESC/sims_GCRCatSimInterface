Fixes cut and knots scaling

bin.src/instance_crawler.py
@@ -83,10 +83,10 @@ def fix_disk_knots(in_instcat_disk, in_instcat_knots,
                     disk_internal_rv = 0
 
                 # If the galaxy is offensive, clip the av and rv values
-                if disk_internal_av < 0 or disk_internal_rv > 1:
+                if disk_internal_av < 0 or disk_internal_rv < 1:
                     #print('Fixing offensive disk %d with av/rv extinction: %f/%f'%(id_disk,disk_internal_av, disk_internal_rv))
                     disk_internal_av = np.clip(disk_internal_av,0.0,None)
-                    disk_internal_rv = np.clip(disk_internal_rv,None,1.0)
+                    disk_internal_rv = np.clip(disk_internal_rv,1.0,None)
                     tokens_disk[18] = ("%.7f"%disk_internal_av).rstrip('0')
                     tokens_disk[19] = ("%.7f"%disk_internal_rv).rstrip('0')
                     count_extinction += 1
@@ -111,7 +111,9 @@ def fix_disk_knots(in_instcat_disk, in_instcat_knots,
             # Apply flux cap for large galaxies
             size = np.float(tokens_disk[13])
             if size > 1.:
-                knots_flux_ratio = np.clip(knots_flux_ratio, 0, 0.5)
+                knots_flux_ratio = 0.5*(1 - np.tanh(0.5*np.log(size)))
+
+                 np.clip(knots_flux_ratio, 0, 0.5)
                 count_knots+=1
                 #print("Capping knots flux for object %d, with magnorm: %f and size %f"%(id_knots,magnorm_disk,size))
 
@@ -154,12 +156,13 @@ def fix_bulge(in_instcat_bulge, out_instcat_bulge):
                 internal_rv = 0
 
             # If the galaxy is offensive, clip the av and rv values
-            if internal_av < 0 or internal_rv > 1:
+            if internal_av < 0 or internal_rv < 1:
                 #print('Fixing offensive bulge %d with av/rv extinction: %f/%f'%(id_bulge,internal_av, internal_rv))
                 internal_av = np.clip(internal_av,0.0,None)
-                internal_rv = np.clip(internal_rv,None,1.0)
+                internal_rv = np.clip(internal_rv,1.0,None)
                 tokens_bulge[18] = ("%.7f"%internal_av).rstrip('0')
                 tokens_bulge[19] = ("%.7f"%internal_rv).rstrip('0')
+                count_extinction += 1
 
             line_bulge = ' '.join(tokens_bulge)
             output_bulge.write(line_bulge.strip()+'\n')