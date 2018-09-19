2fc7b0ddaf8c23e7f34874e28a61adc422582a8f
danielsf <scott.f.daniel@gmail.com>
2018-09-19 09:46:17 -0700

    add plotting utils

---
+++ workspace/sed_cache/plotting_utils.py
@@ -0,0 +1,75 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+import os
+import sqlite3
+import h5py
+
+import numpy as np
+
+__all__ = ["plot_color_mesh"]
+
+def make_2d_histogram(xx, yy, dx, dy):
+    """
+    returns indices and counts of unique points on the map
+    """
+    i_color1 = np.round(xx/dx).astype(int)
+    i_color2 = np.round(yy/dy).astype(int)
+    dex_reverse = np.array([i_color1, i_color2])
+    dex_arr = dex_reverse.transpose()
+    # see http://stackoverflow.com/questions/16970982/find-unique-rows-in-numpy-array
+    dex_raw = np.ascontiguousarray(dex_arr).view(np.dtype((np.void, dex_arr.dtype.itemsize*dex_arr.shape[1])))
+    _, unique_rows, unique_counts = np.unique(dex_raw, return_index=True, return_counts=True)
+
+    return unique_rows, unique_counts
+
+
+def plot_color(xx, yy, dx, dy):
+    dexes, cts = make_2d_histogram(xx, yy, dx, dy)
+    sorted_dex = np.argsort(cts)
+    dexes = dexes[sorted_dex]
+    cts = cts[sorted_dex]
+    plt.scatter(xx[dexes], yy[dexes], c=cts, s=5,
+                cmap=plt.cm.gist_ncar, edgecolor='')
+
+    plt.colorbar()
+
+
+def plot_color_mesh(xx, yy, dx, dy, vmin=None, vmax=None):
+    i_x_arr = np.round((xx-xx.min())/dx).astype(int)
+    i_y_arr = np.round((yy-yy.min())/dy).astype(int)
+    new_x = i_x_arr*dx
+    new_y = i_y_arr*dy
+    dex_list, ct_list = make_2d_histogram(new_x, new_y, dx, dy)
+
+    if i_x_arr.min()<0 or i_y_arr.min()<0:
+        raise RuntimeError('negative dex %e %d %e %d' %
+                           (xx.min(), i_x_arr.min(), yy.min(), i_y_arr.min()))
+
+    x_mesh=np.arange(xx.min(),xx.max()+0.1,dx)
+    y_mesh=np.arange(yy.min(),yy.max()+0.1,dy)
+    x_mesh,y_mesh = np.meshgrid(x_mesh,y_mesh,indexing='xy')
+    z_mesh = np.zeros(shape=x_mesh.shape, dtype=int)
+    ct_1000b = 0
+
+    for dex, ct in zip(dex_list, ct_list):
+        ix = i_x_arr[dex]
+        iy = i_y_arr[dex]
+        z_mesh[iy][ix] += ct
+
+    z_mesh = np.ma.masked_where(z_mesh==0,z_mesh)
+    plt.pcolormesh(x_mesh,y_mesh,z_mesh, vmin=vmin, vmax=vmax)
+                   #norm=matplotlib.colors.LogNorm(vmin=1.0,
+                   #                               vmax=1.2e6))
+    cbar = plt.colorbar()
+    cbar.set_label('sources per pixel', size=20)
+    cbar.ax.tick_params(labelsize=20)
+    #cbar.set_ticklabels(size=20)
+
+    ct_1000 = 0
+    big_min = np.round((2.8-xx.min())/dx).astype(int)
+    big_max = x_mesh.shape[0]
+
+
