0b4d11c8c926105104e76cf513d25d0dae2c9e95
danielsf <scott.f.daniel@gmail.com>
2018-10-03 14:40:34 -0700

    make add_sne.py run on arbitrary db files

--- workspace/truth/add_sne.py
+++ workspace/truth/add_sne.py
@@ -1,10 +1,14 @@
 from ExtractSNeParams import add_unsprinkled_sne_params
 import os
+import argparse
 
-db_dir = '/astro/store/pogo3/danielsf/truth_181002'
-assert os.path.isdir(db_dir)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--param_db', type=str, default=None,
+                        help='Path to the sqlite database to which you '
+                        'wish to add unsprinkled supernova parameters')
 
-db_file = os.path.join(db_dir, 'truth_params_181002_protodc2_v3.db')
-assert os.path.isfile(db_file)
+    if args.param_db is None:
+        raise RuntimeError("Must specify param_db")
 
-add_unsprinkled_sne_params(db_file)
+    add_unsprinkled_sne_params(args.param_db)
