f668210463aa3643f9d7c680f3c21dc9da390dea
danielsf <scott.f.daniel@gmail.com>
2018-10-23 10:13:12 -0700

    add ra, dec to sed lookup files

--- workspace/sed_cache/convert_h5_files.py
+++ workspace/sed_cache/convert_h5_files.py
@@ -3,6 +3,8 @@ import h5py
 import os
 import numpy as np
 import time
+import GCRCatalogs
+from GCR import GCRQuery
 
 data_dir = os.path.join(os.environ['SCRATCH'], 'sed_cache_181017')
 assert os.path.isdir(data_dir)
@@ -22,11 +24,20 @@ for i_nn, nn in enumerate(list_of_files):
 
 hp_list = [10069, 10196, 9940, 10068, 10195, 10197, 10323, 10324, 10325, 10447, 10448]
 
+cat = GCRCatalogs.load_catalog('cosmoDC2_v1.0_image')
+
 t_start = time.time()
 for hp in hp_list:
     print('processing %d; %.1e minutes' % (hp, (time.time()-t_start)/60.0))
     in_file_name = os.path.join(data_dir, 'sed_fit_%d.h5' % hp)
     assert os.path.isfile(in_file_name)
+
+    hp_query = GCRQuery('healpix_pixel==%d' % hp)
+    cat_qties = cat.get_quantities(['galaxy_id', 'ra', 'dec'], native_filters=[hp_query])
+
+    sorted_dex = np.argsort(cat_qties['galaxy_id'])
+    for k in cat_qties.keys():
+        cat_qties[k] = cat_qties[k][sorted_dex]
 
     with h5py.File(in_file_name, 'r') as in_file:
         disk_names = in_file['disk_sed'].value.astype(str)
@@ -35,6 +46,10 @@ for hp in hp_list:
         bulge_names = in_file['bulge_sed'].value.astype(str)
         bulge_name_idx = np.array([sed_name_dict[nn] for nn in bulge_names])
         print('    got bulges')
+
+        mapped_dex = np.searchsorted(cat_qties['galaxy_id'], in_file['galaxy_id'].value)
+        np.testing.assert_array_equal(cat_qties['galaxy_id'][mapped_dex],
+                                      in_file['galaxy_id'].value)
 
         out_file_name = os.path.join(data_dir, 'sed_fit_idx_%d.h5' % hp)
         with h5py.File(out_file_name, 'w') as out_file:
@@ -45,3 +60,5 @@ for hp in hp_list:
             d_sed = out_file.create_dataset('disk_sed', data=disk_name_idx)
             b_sed = out_file.create_dataset('bulge_sed', data=bulge_name_idx)
             out_file.create_dataset('sed_names', data=name_array)
+            out_file.create_dataset('ra', data=cat_qties['ra'][mapped_dex])
+            out_file.create_dataset('dec', data=cat_qties['dec'][mapped_dex])
