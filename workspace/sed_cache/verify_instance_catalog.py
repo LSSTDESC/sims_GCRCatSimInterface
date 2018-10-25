cfeecdafad2f3ab3ab9c928b4a5f5fd5d02420b3
danielsf <scott.f.daniel@gmail.com>
2018-10-25 10:32:40 -0700

    add main module to verify_instance_catalog.py

--- workspace/sed_cache/verify_instance_catalog.py
+++ workspace/sed_cache/verify_instance_catalog.py
@@ -37,153 +37,158 @@ def get_sed(name, magnorm, redshift, av, rv):
     ss.redshiftSED(redshift, dimming=True)
     return ss
 
-colnames = ['obj', 'uniqueID', 'ra', 'dec', 'magnorm', 'sed', 'redshift', 'g1', 'g2',
-            'kappa', 'dra', 'ddec', 'src_type', 'major', 'minor',
-            'positionAngle', 'sindex', 'dust_rest', 'rest_av', 'rest_rv',
-            'dust_obs', 'obs_av', 'obs_rv']
+if __name__ == "__main__":
 
-col_types = {'magnorm': float, 'redshift': float,
-             'rest_av': float, 'rest_rv': float,
-             'sed': bytes, 'uniqueID': int}
+    colnames = ['obj', 'uniqueID', 'ra', 'dec', 'magnorm', 'sed', 'redshift', 'g1', 'g2',
+                'kappa', 'dra', 'ddec', 'src_type', 'major', 'minor',
+                'positionAngle', 'sindex', 'dust_rest', 'rest_av', 'rest_rv',
+                'dust_obs', 'obs_av', 'obs_rv']
 
-data_dir = os.path.join(os.environ['SCRATCH'], 'instcat_181024_verify', '00277065')
-assert os.path.isdir(data_dir)
+    col_types = {'magnorm': float, 'redshift': float,
+                 'rest_av': float, 'rest_rv': float,
+                 'sed': bytes, 'uniqueID': int}
 
-phosim_file = os.path.join(data_dir, 'phosim_cat_277065.txt')
-assert os.path.isfile(phosim_file)
-bandpass_name = None
-bandpass_name_list = 'ugrizy'
-with open(phosim_file, 'r') as in_file:
-    for line in in_file:
-        params = line.strip().split()
-        if params[0] == 'filter':
-            bandpass_name = bandpass_name_list[int(params[1])]
+    data_dir = os.path.join(os.environ['SCRATCH'], 'instcat_181024_verify', '00277065')
+    assert os.path.isdir(data_dir)
 
-assert bandpass_name is not None
+    phosim_file = os.path.join(data_dir, 'phosim_cat_277065.txt')
+    assert os.path.isfile(phosim_file)
+    bandpass_name = None
+    bandpass_name_list = 'ugrizy'
+    with open(phosim_file, 'r') as in_file:
+        for line in in_file:
+            params = line.strip().split()
+            if params[0] == 'filter':
+                bandpass_name = bandpass_name_list[int(params[1])]
 
-(tot_dict,
- hw_dict) = BandpassDict.loadBandpassesFromFiles()
+    assert bandpass_name is not None
 
-bandpass = hw_dict[bandpass_name]
-nrows = 10000
+    (tot_dict,
+     hw_dict) = BandpassDict.loadBandpassesFromFiles()
 
-disk_file = os.path.join(data_dir, 'disk_gal_cat_277065.txt.gz')
-assert os.path.isfile(disk_file)
+    bandpass = hw_dict[bandpass_name]
+    nrows = 10000
 
-bulge_file = os.path.join(data_dir, 'bulge_gal_cat_277065.txt.gz')
-assert os.path.isfile(bulge_file)
+    disk_file = os.path.join(data_dir, 'disk_gal_cat_277065.txt.gz')
+    assert os.path.isfile(disk_file)
 
-knots_file = os.path.join(data_dir, 'knots_cat_277065.txt.gz')
-assert os.path.isfile(knots_file)
+    bulge_file = os.path.join(data_dir, 'bulge_gal_cat_277065.txt.gz')
+    assert os.path.isfile(bulge_file)
 
-disk_df = pd.read_csv(disk_file, delimiter=' ',
-                        compression='gzip', names=colnames, dtype=col_types, nrows=None)
+    knots_file = os.path.join(data_dir, 'knots_cat_277065.txt.gz')
+    assert os.path.isfile(knots_file)
 
-disk_df['galaxy_id'] = pd.Series(disk_df['uniqueID']//1024, index=disk_df.index)
-disk_df = disk_df.set_index('galaxy_id')
+    disk_df = pd.read_csv(disk_file, delimiter=' ',
+                          compression='gzip', names=colnames, dtype=col_types, nrows=None)
 
-bulge_df = pd.read_csv(bulge_file, delimiter=' ',
-                       compression='gzip', names=colnames, dtype=col_types, nrows=None)
-bulge_df['galaxy_id'] = pd.Series(bulge_df['uniqueID']//1024, index=bulge_df.index)
-bulge_df = bulge_df.set_index('galaxy_id')
+    disk_df['galaxy_id'] = pd.Series(disk_df['uniqueID']//1024, index=disk_df.index)
+    disk_df = disk_df.set_index('galaxy_id')
 
-for ii in range(len(colnames)):
-    colnames[ii] = colnames[ii]+'_knots'
+    bulge_df = pd.read_csv(bulge_file, delimiter=' ',
+                           compression='gzip', names=colnames, dtype=col_types, nrows=None)
+    bulge_df['galaxy_id'] = pd.Series(bulge_df['uniqueID']//1024, index=bulge_df.index)
+    bulge_df = bulge_df.set_index('galaxy_id')
 
-knots_df = pd.read_csv(knots_file, delimiter=' ',
-                       compression='gzip', names=colnames, dtype=col_types, nrows=None)
-knots_df['galaxy_id'] = pd.Series(knots_df['uniqueID_knots']//1024, index=knots_df.index)
-knots_df = knots_df.set_index('galaxy_id')
+    for ii in range(len(colnames)):
+        colnames[ii] = colnames[ii]+'_knots'
 
-wanted_col = ['sed', 'magnorm', 'redshift', 'rest_av', 'rest_rv', 'ra', 'dec']
-galaxy_df = disk_df[wanted_col].join(bulge_df[wanted_col], how='outer', lsuffix='_disk', rsuffix='_bulge')
-for ii in range(len(wanted_col)):
-    wanted_col[ii] = wanted_col[ii]+'_knots'
-galaxy_df = galaxy_df.join(knots_df[wanted_col], how='outer', rsuffix='_knots')
+    knots_df = pd.read_csv(knots_file, delimiter=' ',
+                           compression='gzip', names=colnames, dtype=col_types, nrows=None)
+    knots_df['galaxy_id'] = pd.Series(knots_df['uniqueID_knots']//1024, index=knots_df.index)
+    knots_df = knots_df.set_index('galaxy_id')
 
-rng = np.random.RandomState(9999)
-dexes = rng.choice(galaxy_df.index.values, size=nrows, replace=False)
+    wanted_col = ['sed', 'magnorm', 'redshift', 'rest_av', 'rest_rv', 'ra', 'dec']
+    galaxy_df = disk_df[wanted_col].join(bulge_df[wanted_col], how='outer',
+                                         lsuffix='_disk', rsuffix='_bulge')
+    for ii in range(len(wanted_col)):
+        wanted_col[ii] = wanted_col[ii]+'_knots'
+    galaxy_df = galaxy_df.join(knots_df[wanted_col], how='outer', rsuffix='_knots')
 
-galaxy_df = galaxy_df.loc[dexes]
+    rng = np.random.RandomState(9999)
+    dexes = rng.choice(galaxy_df.index.values, size=nrows, replace=False)
 
-galaxy_df = galaxy_df.sort_index()
+    galaxy_df = galaxy_df.loc[dexes]
 
-ra_center = np.nanmedian(galaxy_df['ra_disk'].values)
-dec_center = np.nanmedian(galaxy_df['dec_disk'].values)
+    galaxy_df = galaxy_df.sort_index()
 
-dd = angularSeparation(ra_center, dec_center, galaxy_df['ra_disk'].values, galaxy_df['dec_disk'].values)
-radius_deg = np.nanmax(dd)
-ra_rad = np.radians(ra_center)
-dec_rad = np.radians(dec_center)
-vv = np.array([np.cos(ra_rad)*np.cos(dec_rad),
-               np.sin(ra_rad)*np.cos(dec_rad),
-               np.sin(dec_rad)])
+    ra_center = np.nanmedian(galaxy_df['ra_disk'].values)
+    dec_center = np.nanmedian(galaxy_df['dec_disk'].values)
 
-healpix_list = healpy.query_disc(32, vv, np.radians(radius_deg),
-                                 nest=False, inclusive=True)
+    dd = angularSeparation(ra_center, dec_center,
+                           galaxy_df['ra_disk'].values,
+                           galaxy_df['dec_disk'].values)
+    radius_deg = np.nanmax(dd)
+    ra_rad = np.radians(ra_center)
+    dec_rad = np.radians(dec_center)
+    vv = np.array([np.cos(ra_rad)*np.cos(dec_rad),
+                   np.sin(ra_rad)*np.cos(dec_rad),
+                   np.sin(dec_rad)])
 
-print('healpix list')
-print(healpix_list)
-print(ra_center, dec_center)
+    healpix_list = healpy.query_disc(32, vv, np.radians(radius_deg),
+                                     nest=False, inclusive=True)
 
-hp_query = GCRQuery()
-for hp in healpix_list:
-    hp_query |= GCRQuery('healpix_pixel==%d' % hp)
+    print('healpix list')
+    print(healpix_list)
+    print(ra_center, dec_center)
 
-print('len(galaxy_df) ',len(galaxy_df))
-print('built final df')
-cat = GCRCatalogs.load_catalog('cosmoDC2_v1.0_image')
-gid = cat.get_quantities('galaxy_id', native_filters=[hp_query])['galaxy_id']
-print('loaded galaxy_id')
-valid_dexes = np.where(np.in1d(gid, galaxy_df.index.values,assume_unique=True))
-print('got valid_dexes')
-print(len(valid_dexes[0]))
-print(len(galaxy_df))
+    hp_query = GCRQuery()
+    for hp in healpix_list:
+        hp_query |= GCRQuery('healpix_pixel==%d' % hp)
 
-sorted_dex = np.argsort(gid[valid_dexes])
-valid_dexes = valid_dexes[0][sorted_dex]
-gid = gid[valid_dexes]
+    print('len(galaxy_df) ',len(galaxy_df))
+    print('built final df')
+    cat = GCRCatalogs.load_catalog('cosmoDC2_v1.0_image')
+    gid = cat.get_quantities('galaxy_id', native_filters=[hp_query])['galaxy_id']
+    print('loaded galaxy_id')
+    valid_dexes = np.where(np.in1d(gid, galaxy_df.index.values,assume_unique=True))
+    print('got valid_dexes')
+    print(len(valid_dexes[0]))
+    print(len(galaxy_df))
 
-np.testing.assert_array_equal(gid, galaxy_df.index.values)
+    sorted_dex = np.argsort(gid[valid_dexes])
+    valid_dexes = valid_dexes[0][sorted_dex]
+    gid = gid[valid_dexes]
 
-mag_name = 'mag_true_%s_lsst' % bandpass_name
-mags = cat.get_quantities(mag_name, native_filters=[hp_query])[mag_name][valid_dexes]
+    np.testing.assert_array_equal(gid, galaxy_df.index.values)
 
-d_mag_max = -1.0
+    mag_name = 'mag_true_%s_lsst' % bandpass_name
+    mags = cat.get_quantities(mag_name, native_filters=[hp_query])[mag_name][valid_dexes]
 
-for g, mag_true, (index, row) in zip(gid, mags, galaxy_df.iterrows()):
-    assert g==index
+    d_mag_max = -1.0
 
-    if np.isnan(row['magnorm_disk']):
-        disk_flux = 0.0
-    else:
-        ss = get_sed(row['sed_disk'], row['magnorm_disk'],
-                     row['redshift_disk'], row['rest_av_disk'],
-                     row['rest_rv_disk'])
-        disk_mag = ss.calcMag(bandpass)
-        disk_flux = np.power(10.0,-0.4*disk_mag)
+    for g, mag_true, (index, row) in zip(gid, mags, galaxy_df.iterrows()):
+        assert g==index
 
-    if np.isnan(row['magnorm_bulge']):
-        bulge_flux = 0.0
-    else:
-        ss = get_sed(row['sed_bulge'], row['magnorm_bulge'],
-                     row['redshift_bulge'], row['rest_av_bulge'],
-                     row['rest_rv_bulge'])
-        bulge_mag = ss.calcMag(bandpass)
-        bulge_flux = np.power(10.0,-0.4*bulge_mag)
+        if np.isnan(row['magnorm_disk']):
+            disk_flux = 0.0
+        else:
+            ss = get_sed(row['sed_disk'], row['magnorm_disk'],
+                         row['redshift_disk'], row['rest_av_disk'],
+                         row['rest_rv_disk'])
+            disk_mag = ss.calcMag(bandpass)
+            disk_flux = np.power(10.0,-0.4*disk_mag)
 
-    if np.isnan(row['magnorm_knots']):
-        knots_flux = 0.0
-    else:
-        ss = get_sed(row['sed_knots'], row['magnorm_knots'],
-                     row['redshift_knots'], row['rest_av_knots'],
-                     row['rest_rv_knots'])
-        knots_mag = ss.calcMag(bandpass)
-        knots_flux = np.power(10.0,-0.4*knots_mag)
+        if np.isnan(row['magnorm_bulge']):
+            bulge_flux = 0.0
+        else:
+            ss = get_sed(row['sed_bulge'], row['magnorm_bulge'],
+                         row['redshift_bulge'], row['rest_av_bulge'],
+                         row['rest_rv_bulge'])
+            bulge_mag = ss.calcMag(bandpass)
+            bulge_flux = np.power(10.0,-0.4*bulge_mag)
 
-    tot_mag = -2.5*np.log10(disk_flux+bulge_flux+knots_flux)
-    d_mag = np.abs(tot_mag-mag_true)
-    if d_mag>d_mag_max:
-        d_mag_max = d_mag
-        print('d_mag_max %e -- InstanceCatalgo %e truth %e' % (d_mag_max, tot_mag, mag_true))
-        #print(row)
+        if np.isnan(row['magnorm_knots']):
+            knots_flux = 0.0
+        else:
+            ss = get_sed(row['sed_knots'], row['magnorm_knots'],
+                         row['redshift_knots'], row['rest_av_knots'],
+                         row['rest_rv_knots'])
+            knots_mag = ss.calcMag(bandpass)
+            knots_flux = np.power(10.0,-0.4*knots_mag)
+
+        tot_mag = -2.5*np.log10(disk_flux+bulge_flux+knots_flux)
+        d_mag = np.abs(tot_mag-mag_true)
+        if d_mag>d_mag_max:
+            d_mag_max = d_mag
+            print('d_mag_max %e -- InstanceCatalgo %e truth %e' % (d_mag_max, tot_mag, mag_true))
+            #print(row)
