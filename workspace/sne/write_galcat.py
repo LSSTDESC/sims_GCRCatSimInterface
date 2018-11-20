6be97d1d10d8280666ca220fcf803362f92063f3
danielsf <scott.f.daniel@gmail.com>
2018-11-20 10:28:03 -0800

    specify catalog and out_dir at command line

--- workspace/sne/write_galcat.py
+++ workspace/sne/write_galcat.py
@@ -1,6 +1,7 @@
 import sys
 import GCRCatalogs
 import os
+import argparse
 
 ## check version
 #print('GCRCatalogs =', GCRCatalogs.__version__, '|' ,'GCR =', GCRCatalogs.GCR.__version__)
@@ -11,8 +12,8 @@ import os
 import pandas as pd
 
 
-def write_hdf(healpixel, out_dir):
-    gc = GCRCatalogs.load_catalog('cosmoDC2_v1.0_image',
+def write_hdf(healpixel, out_dir, catalog_name):
+    gc = GCRCatalogs.load_catalog(catalog_name,
                               {'healpix_pixels': [healpixel]})
 
     x = gc.get_quantities(['galaxy_id', 'stellar_mass', 'stellar_mass_disk', 'stellar_mass_bulge',
@@ -23,9 +24,13 @@ def write_hdf(healpixel, out_dir):
 
 
 if __name__ == "__main__":
-    cat_config = GCRCatalogs.get_catalog_config('cosmoDC2_v1.0_image')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--catalog', type=str, default=None)
+    parser.add_argument('--out_dir', type=str, default=None)
+    args = parser.parse_args()
+
+    cat_config = GCRCatalogs.get_catalog_config(args.catalog)
     healpix_list = cat_config['healpix_pixels']
-    out_dir = os.path.join(os.environ['SCRATCH'], 'sne_production')
-    assert os.path.isdir(out_dir)
+    assert os.path.isdir(args.out_dir)
     for hh in healpix_list:
-        write_hdf(hh, out_dir)
+        write_hdf(hh, args.out_dir, args.catalog)
