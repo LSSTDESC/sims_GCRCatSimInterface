74502f27a927b0ae0edbf27c3c172c0e513dac96
danielsf <scott.f.daniel@gmail.com>
2018-11-21 19:50:33 -0800

    update slurm script generation for run 2.1

--- workspace/run2.1/generate_slurm_script.py
+++ workspace/run2.1/generate_slurm_script.py
@@ -5,11 +5,13 @@ import numpy as np
 
 if __name__ == "__main__":
 
+    config_file = 'config_file_2.1.wfd.json'
+    out_dir_root = '$SCRATCH/instcat2.1_batch_'
+    out_name_root = 'slurm_scripts/batch_script_'
+
+
     n_obs = int(sys.argv[1])
     d_obs = int(sys.argv[2])
-    out_name = sys.argv[3]
-    if os.path.exists(out_name):
-        raise RuntimeError("%s exists" % out_name)
 
     obs_hist_id = []
     with open('data/wfd_obshistid_list.txt', 'r') as in_file:
@@ -19,25 +21,39 @@ if __name__ == "__main__":
 
     obs_hist_id = np.array(obs_hist_id)
     rng = np.random.RandomState(88123)
-    obs_to_simulate = rng.choice(obs_hist_id, n_obs, replace=False)
+    rng.shuffle(obs_hist_id)
 
-    with open(out_name, 'w') as out_file:
-        with open('header.txt', 'r') as in_file:
-            for line in in_file:
-                out_file.write(line)
+    for i_file, i_start in enumerate(range(0,len(obs_hist_id), n_obs)):
+        batch_slice = slice(i_start, i_start+n_obs)
+        batch = obs_hist_id[batch_slice]
+        out_name = out_name_root+'%d.sl' % i_file
+        out_dir = out_dir_root+'%d.sl' % i_file
+        with open(out_name, 'w') as out_file:
+            n_srun = int(np.ceil(len(batch)/d_obs))
+            out_file.write('#! /bin/bash/ -l\n')
+            out_file.write('#SBATCH -N %d\n' % n_srun)
+            out_file.write('#SBATCH -o slurm_output/batch_%d_out.txt\n' % i_file)
+            out_file.write('#SBATCH -e slurm_err/batch_%d_err.txt\n' % i_file)
+            with open('header.txt', 'r') as in_file:
+                for line in in_file:
+                    out_file.write(line)
 
-        out_file.write('\n')
-
-        batches = len(obs_to_simulate)/d_obs
-        print('batches %e' % batches)
-        for i_start in range(0, len(obs_to_simulate), d_obs):
-            s = slice(i_start, i_start+d_obs)
-            these_obs = obs_to_simulate[s]
             out_file.write('\n')
-            out_file.write('srun -N 1 -n 1 -c 24 --exclusive \\\n')
-            out_file.write('bash instcat_runner.sh ${out_dir} ${config_file}')
-            for ii in these_obs:
-                out_file.write(' %d' % ii)
-            out_file.write(' &\n\n')
+            out_file.write('out_dir=%s\n' % out_dir)
+            out_file.write('config_file=%s\n' % config_file)
+            out_file.write("if [ ! -d ${out_dir} ]; then\n")
+            out_file.write("    mkdir -p ${out_dir}\n")
+            out_file.write("fi\n")
+            out_file.write('\n')
 
-        out_file.write('\nwait\n')            
+            for i_0 in range(0, len(batch), d_obs):
+                s = slice(i_0, i_0+d_obs)
+                these_obs = batch[s]
+                out_file.write('\n')
+                out_file.write('srun -N 1 -n 1 -c 24 --exclusive \\\n')
+                out_file.write('bash instcat_runner.sh ${out_dir} ${config_file}')
+                for ii in these_obs:
+                    out_file.write(' %d' % ii)
+                out_file.write(' &\n\n')
+
+            out_file.write('\nwait\n')
