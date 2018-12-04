dd7f95626afa409f3e0dfd58631874caeaa9557d
danielsf <scott.f.daniel@gmail.com>
2018-12-04 10:48:06 -0800

    use argparse in generate_slurm_scripts.py

--- workspace/run2.1/generate_slurm_script.py
+++ workspace/run2.1/generate_slurm_script.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import numpy as np
-
+import argparse
 
 if __name__ == "__main__":
 
@@ -10,8 +10,13 @@ if __name__ == "__main__":
     out_name_root = 'slurm_scripts/batch_script_'
 
 
-    n_obs = int(sys.argv[1])
-    d_obs = int(sys.argv[2])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n_obs', type=int, default=1000,
+                        help='Number of pointings per slurm script '
+                        '(default=1000)')
+    parser.add_argument('--d_obs', type=int, default=7,
+                        help='Number of pointings per node (default=7)')
+    args = parser.parse_args()
 
     obs_hist_id = []
     with open('data/wfd_obshistid_list.txt', 'r') as in_file:
@@ -23,13 +28,13 @@ if __name__ == "__main__":
     rng = np.random.RandomState(88123)
     rng.shuffle(obs_hist_id)
 
-    for i_file, i_start in enumerate(range(0,len(obs_hist_id), n_obs)):
-        batch_slice = slice(i_start, i_start+n_obs)
+    for i_file, i_start in enumerate(range(0,len(obs_hist_id), args.n_obs)):
+        batch_slice = slice(i_start, i_start+args.n_obs)
         batch = obs_hist_id[batch_slice]
         out_name = out_name_root+'%d.sl' % i_file
         out_dir = out_dir_root+'%d' % i_file
         with open(out_name, 'w') as out_file:
-            n_srun = int(np.ceil(len(batch)/d_obs))
+            n_srun = int(np.ceil(len(batch)/args.d_obs))
             out_file.write('#!/bin/bash -l\n')
             out_file.write('#SBATCH -N %d\n' % n_srun)
             out_file.write('#SBATCH -o slurm_out/batch_%d_out.txt\n' % i_file)
@@ -46,8 +51,8 @@ if __name__ == "__main__":
             out_file.write("fi\n")
             out_file.write('\n')
 
-            for i_0 in range(0, len(batch), d_obs):
-                s = slice(i_0, i_0+d_obs)
+            for i_0 in range(0, len(batch), args.d_obs):
+                s = slice(i_0, i_0+args.d_obs)
                 these_obs = batch[s]
                 out_file.write('\n')
                 out_file.write('srun -N 1 -n 1 -c 24 --exclusive \\\n')
