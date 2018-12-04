3304aca615702c49a42e17aaeeba00b955e23842
danielsf <scott.f.daniel@gmail.com>
2018-12-04 12:52:14 -0800

    allow generate_slurm_script to scan for completed obsHistID

--- workspace/run2.1/generate_slurm_script.py
+++ workspace/run2.1/generate_slurm_script.py
@@ -16,29 +16,64 @@ if __name__ == "__main__":
                         '(default=1000)')
     parser.add_argument('--d_obs', type=int, default=7,
                         help='Number of pointings per node (default=7)')
+    parser.add_argument('--already_done', type=str, nargs='+', default=None,
+                        help='directories to check for already completed '
+                        'InstanceCatalogs')
     args = parser.parse_args()
 
+    obs_already_done = set()
+    if args.already_done is not None:
+        if isinstance(args.already_done, str):
+            args.already_done = [args.already_done]
+
+        for dir_name in args.already_done:
+            print('checking %s' % dir_name)
+            if not os.path.isdir(dir_name):
+                raise RuntimeError("Something is wrong\n%s\nis not a dir"
+                                   % dir_name)
+
+            file_list = os.listdir(dir_name)
+            for file_name in file_list:
+                if not file_name.startswith('job'):
+                    continue
+                pars = file_name.replace('.txt','').split('_')
+                obshistid = int(pars[2])
+                with open(os.path.join(dir_name, file_name), 'r') as in_file:
+                    lines = in_file.readlines()
+                    is_done = ('all done' in lines[-1])
+                if is_done:
+                    obs_already_done.add(obshistid)
+
+    print('N already done %d' % (len(obs_already_done)))
     obs_hist_id = []
     with open('data/wfd_obshistid_list.txt', 'r') as in_file:
         for line in in_file:
             ii=int(line.strip())
-            obs_hist_id.append(ii)
+            if ii not in obs_already_done:
+                obs_hist_id.append(ii)
 
     obs_hist_id = np.array(obs_hist_id)
     rng = np.random.RandomState(88123)
     rng.shuffle(obs_hist_id)
 
+    i_file_offset = 0
     for i_file, i_start in enumerate(range(0,len(obs_hist_id), args.n_obs)):
         batch_slice = slice(i_start, i_start+args.n_obs)
         batch = obs_hist_id[batch_slice]
-        out_name = out_name_root+'%d.sl' % i_file
-        out_dir = out_dir_root+'%d' % i_file
+        out_dir = None
+        while out_dir is None or os.path.exists(out_dir):
+            out_name = out_name_root+'%d.sl' % (i_file+i_file_offset)
+            out_dir = out_dir_root+'%d' % (i_file+i_file_offset)
+            if os.path.exists(out_dir):
+                i_file_offset += 1
         with open(out_name, 'w') as out_file:
             n_srun = int(np.ceil(len(batch)/args.d_obs))
             out_file.write('#!/bin/bash -l\n')
             out_file.write('#SBATCH -N %d\n' % n_srun)
-            out_file.write('#SBATCH -o slurm_out/batch_%d_out.txt\n' % i_file)
-            out_file.write('#SBATCH -e slurm_err/batch_%d_err.txt\n' % i_file)
+            out_file.write('#SBATCH -o slurm_out/batch_%d_out.txt\n' %
+                           (i_file+i_file_offset))
+            out_file.write('#SBATCH -e slurm_err/batch_%d_err.txt\n' %
+                           (i_file+i_file_offset))
             with open('header.txt', 'r') as in_file:
                 for line in in_file:
                     out_file.write(line)
