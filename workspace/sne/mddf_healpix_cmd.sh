9c905c0a066de790e7713cea82f6e716e611e869
danielsf <scott.f.daniel@gmail.com>
2018-12-11 14:07:18 -0800

    do not specify healpixel in DDF script

--- workspace/sne/mddf_healpix_cmd.sh
+++ workspace/sne/mddf_healpix_cmd.sh
@@ -1,4 +1,4 @@
 sne_dir=$SCRATCH/cosmoDC2_v1.1.4_sne/
 python write_healpixel.py --out_dir ${sne_dir}sne_csv_mddf \
 --data_root ${sne_dir}gal_hdf5_mddf \
---survey mDDF --zmax 1.4 --healpixelId 9043
+--survey mDDF --zmax 1.4
