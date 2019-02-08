04477b8d88286f4ead14fb2291afbe92c4357bcf
danielsf <scott.f.daniel@gmail.com>
2019-02-07 21:57:11 -0800

    sne_pipeline.py scans for vanishing SNe

--- workspace/validation/sne_pipeline.py
+++ workspace/validation/sne_pipeline.py
@@ -3,17 +3,11 @@ import numpy as np
 
 from ic_sne import validate_sne
 
+import multiprocessing
+
 import time
 
-import argparse
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--in_list', type=str, default=None)
-    parser.add_argument('--out_file', type=str, default=None)
-
-    args = parser.parse_args()
+def scan_sne(in_list, out_file):
 
     project_dir = os.path.join('/global/projecta/projectdirs',
                                'lsst/groups/SSim/DC2/cosmoDC2_v1.1.4')
@@ -22,9 +16,9 @@ if __name__ == "__main__":
                           'agn_db_mbh7_mi30_sf4.db')
 
     ct = 0
-    with open(args.out_file, 'w') as vanishing_file:
+    with open(out_file, 'w') as vanishing_file:
         vanishing_file.write('# obsid bandpass snid mag c x0 x1 t0 z expmjd expmjd-t0\n')
-        with open(args.in_list, 'r') as in_file:
+        with open(in_list, 'r') as in_file:
             for line in in_file:
                 p = line.strip().split()
                 cat_dir = p[0]
@@ -32,8 +26,24 @@ if __name__ == "__main__":
                 validate_sne(cat_dir, obsid,
                              scatter_file=None,
                              vanishing_file=vanishing_file)
-            ct += 1
-            if ct>=10:
-                break
+                ct += 1
 
-    print('all done with %s' % args.in_list)
+    print('all done with %s' % in_list)
+
+
+if __name__ == "__main__":
+
+    t_start = time.time()
+    p_list = []
+    for ii in range(14):
+        in_name = 'instcat_for_sne_scanning_%d.txt' % ii
+        out_name = 'sne_vanished_%d.txt' % ii
+        p = multiprocessing.Process(target=scan_sne,
+                                    args=(in_name, out_name))
+
+        p.start()
+        p_list.append(p)
+
+    for p in p_list:
+        p.join()
+    print('that took %e' % (time.time()-t_start))
