7c154fe4e7994a0fb4a4013aba1d1f529864a8e9
danielsf <scott.f.daniel@gmail.com>
2019-03-19 15:05:58 -0700

    copy tar file to scratch space and then validate

--- workspace/validation/validation_pipeline.py
+++ workspace/validation/validation_pipeline.py
@@ -6,12 +6,14 @@ import desc.sims.GCRCatSimInterface.validation as validation
 import time
 
 import argparse
+import subprocess
+import shutil
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--seed', type=int, default=98)
-
+    parser.add_argument('--file_name', type=str)
+    parser.add_argument('--seed', type=int, default=4561)
     args = parser.parse_args()
 
     rng = np.random.RandomState(args.seed)
@@ -24,19 +26,32 @@ if __name__ == "__main__":
 
     already_run = set()
 
-    parent_dir = os.path.join(os.environ['SCRATCH'], 'test_validation')
+    scratch_dir = os.path.join(os.environ['SCRATCH'], 'test_validation')
+    assert os.path.isdir(scratch_dir)
 
-    assert os.path.isdir(parent_dir)
+    orig_file = args.file_name
+    p = subprocess.Popen(['cp',orig_file,scratch_dir])
+    p.wait()
+    instcat_tar_name = os.path.basename(orig_file)
+    copied_instcat_name = os.path.join(scratch_dir, instcat_tar_name)
+    untarred_instcat_name = os.path.join(scratch_dir,
+                                         instcat_tar_name.replace('.tar.gz',''))
+    print('tar name: ',instcat_tar_name)
+    p = subprocess.Popen(['tar','-C',scratch_dir,
+                          '-xf',os.path.join(scratch_dir, instcat_tar_name)])
+
+    p.wait()
+    print('done untarring')
 
     log_name = 'validated_catalogs.txt'
 
     while len(already_run)<1:
-        obs_dir = '00749676'
+        obs_dir = instcat_tar_name.replace('.tar.gz','')
         obsid = int(obs_dir)
         if obsid in already_run:
             continue
 
-        cat_dir = os.path.join(parent_dir)
+        cat_dir = os.path.join(scratch_dir)
 
         t_start = time.time()
         #validate_sne(cat_dir, obsid, out_file=f_out)
@@ -50,3 +65,6 @@ if __name__ == "__main__":
         already_run.add(obsid)
         with open(log_name, 'a') as out_file:
             out_file.write('%d validated\n' % obsid)
+
+        os.unlink(copied_instcat_name)
+        shutil.rmtree(untarred_instcat_name)
