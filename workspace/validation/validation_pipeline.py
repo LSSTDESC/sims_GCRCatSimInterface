fc7d4400c0fc77aadf2787228a41173c821fd878
danielsf <scott.f.daniel@gmail.com>
2019-03-19 22:24:45 -0700

    validate subset of instance catalogs in dir

--- workspace/validation/validation_pipeline.py
+++ workspace/validation/validation_pipeline.py
@@ -12,7 +12,7 @@ import shutil
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--file_name', type=str)
+    parser.add_argument('--dir_name', type=str)
     parser.add_argument('--seed', type=int, default=4561)
     args = parser.parse_args()
 
@@ -24,28 +24,36 @@ if __name__ == "__main__":
     agn_db = os.path.join(project_dir,
                           'agn_db_mbh7_mi30_sf4.db')
 
-    already_run = set()
+    contents = os.listdir(args.dir_name)
+    list_of_inst_cats = []
+    for name in contents:
+        if not name.endswith('.tar.gz'):
+            continue
+        list_of_inst_cats.append(os.path.join(args.dir_name, name))
+
 
     scratch_dir = os.path.join(os.environ['SCRATCH'], 'test_validation')
     assert os.path.isdir(scratch_dir)
 
-    orig_file = args.file_name
-    p = subprocess.Popen(['cp',orig_file,scratch_dir])
-    p.wait()
-    instcat_tar_name = os.path.basename(orig_file)
-    copied_instcat_name = os.path.join(scratch_dir, instcat_tar_name)
-    untarred_instcat_name = os.path.join(scratch_dir,
-                                         instcat_tar_name.replace('.tar.gz',''))
-    print('tar name: ',instcat_tar_name)
-    p = subprocess.Popen(['tar','-C',scratch_dir,
-                          '-xf',os.path.join(scratch_dir, instcat_tar_name)])
-
-    p.wait()
-    print('done untarring')
-
     log_name = 'validated_catalogs.txt'
+    already_run = set()
+    while len(already_run)<4:
+        orig_file = rng.choice(list_of_inst_cats, size=1)[0]
+        if orig_file in already_run:
+            continue
+        p = subprocess.Popen(['cp',orig_file,scratch_dir])
+        p.wait()
+        instcat_tar_name = os.path.basename(orig_file)
+        copied_instcat_name = os.path.join(scratch_dir, instcat_tar_name)
+        untarred_instcat_name = os.path.join(scratch_dir,
+                                             instcat_tar_name.replace('.tar.gz',''))
+        print('tar name: ',instcat_tar_name)
+        p = subprocess.Popen(['tar','-C',scratch_dir,
+                              '-xf',os.path.join(scratch_dir, instcat_tar_name)])
 
-    while len(already_run)<1:
+        p.wait()
+        print('done untarring')
+
         obs_dir = instcat_tar_name.replace('.tar.gz','')
         obsid = int(obs_dir)
         if obsid in already_run:
@@ -62,7 +70,7 @@ if __name__ == "__main__":
         validation.validate_instance_catalog_positions(cat_dir, obsid, 2.1)
         validation.validate_agn_mags(cat_dir, obsid, agn_db)
         print('\n\nvalidated agn after %e seconds' % (time.time()-t_start))
-        already_run.add(obsid)
+        already_run.add(orig_file)
         with open(log_name, 'a') as out_file:
             out_file.write('%d validated\n' % obsid)
 
