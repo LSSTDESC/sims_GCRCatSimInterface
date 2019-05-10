31208ff0fdd2f3f8cbb756848d5bf8f9ea4f05f4
danielsf <scott.f.daniel@gmail.com>
2019-05-10 09:24:04 -0700

    widen border on stars so that edge CCDs can be calibrated

--- workspace/reference_catalogs/make_reference_catalog.py
+++ workspace/reference_catalogs/make_reference_catalog.py
@@ -129,10 +129,10 @@ if __name__ == "__main__":
 
         bp_dict = photUtils.BandpassDict.loadTotalBandpassesFromFiles()
 
-        ra_min=49.91
-        ra_max=73.80
-        dec_min=-44.32
-        dec_max=-27.24
+        ra_min=47.91
+        ra_max=75.80
+        dec_min=-45.32
+        dec_max=-26.24
 
         where_clause = 'WHERE ra>=%.2f AND ra<=%.2f ' % (ra_min, ra_max)
         where_clause += 'AND decl>=%.2f AND decl<=%.2f' % (dec_min, dec_max)
