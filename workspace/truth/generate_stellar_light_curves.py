a2072c055c04da15f0a5421e34bec92cdde1f93f
danielsf <scott.f.daniel@gmail.com>
2019-10-11 15:10:51 -0700

    start incorporating focal plane model

--- workspace/truth/generate_stellar_light_curves.py
+++ workspace/truth/generate_stellar_light_curves.py
@@ -3,10 +3,13 @@ import sqlite3
 import h5py
 import numpy as np
 
+from lsst.sims.utils import angularSeparation
+from lsst.sims.utils import getRotSkyPos
 from lsst.sims.utils import defaultSpecMap
 import lsst.sims.photUtils as sims_photUtils
 from lsst.sims.utils import ObservationMetaData
 import lsst.sims.catUtils.mixins.VariabilityMixin as variability
+from lsst.sims.coordUtils import chipNameFromRaDecLSST
 
 import multiprocessing
 import time
@@ -77,7 +80,7 @@ def do_photometry(chunk, my_lock, output_dict):
     assert os.path.isfile(htmid_lookup_name)
 
     metadata_dict = {}
-    metadata_keys = ['obsHistID', 'ra', 'dec', 'rotTelPos', 'mjd']
+    metadata_keys = ['obsHistID', 'ra', 'dec', 'rotTelPos', 'mjd', 'filter']
     htmid = int(chunk[0][1])
     with h5py.File(htmid_lookup_name, 'r') as in_file:
         valid_obsid = in_file['%d' % htmid][()]
@@ -89,15 +92,48 @@ def do_photometry(chunk, my_lock, output_dict):
     np.testing.assert_array_equal(metadata_dict['obsHistID'][valid_dex],
                                   valid_obsid)
 
-    expmjd_arr = metadata_dict['mjd'][valid_dex]
+    for k in metadata_dict.keys():
+        metadata_dict[k] = metadata_dict[k][valid_dex]
 
+    t_start = time.time()
     var_gen = VariabilityGenerator(chunk)
     dmag = var_gen.applyVariability(var_gen.varParamStr,
-                                    expmjd=expmjd_arr)
+                                    expmjd=metadata_dict['mjd'])
+
+    t_dmag = time.time()-t_start
+    print('generated dmag in %e seconds' % t_dmag)
 
     print('dmag ',dmag.shape)
-    print('expmjd ',expmjd_arr.shape)    
+    print('expmjd ',metadata_dict['mjd'].shape)
 
+    star_ra = np.array([c[7] for c in chunk])
+    star_dec = np.array([c[8] for c in chunk])
+
+    obs_mask = np.zeros((len(chunk), len(metadata_dict['mjd'])),
+                        dtype=bool)
+    print('made mask')
+
+    fov_radius = 2.1 # in degrees
+    t_start = time.time()
+    for i_obs in range(len(metadata_dict['mjd'])):
+        ra = np.degrees(metadata_dict['ra'][i_obs])
+        dec = np.degrees(metadata_dict['dec'][i_obs])
+
+        ## back-of-the-envelope implementation
+        ## (does not use focal plane model with chip gaps, etc.)
+        #ang_dist = angularSeparation(ra, dec, star_ra, star_dec)
+        #valid = ang_dist<fov_radius
+
+        rotTel = np.degrees(metadata_dict['rotTelPos'][i_obs])
+        obs_md = ObservationMetaData(pointingRA=ra, pointingDec=dec,
+                                     mjd=metadata_dict['mjd'][i_obs])
+        rotSky = getRotSkyPos(ra, dec, obs_md, rotTel)
+        obs_md.rotSkyPos = rotSky
+        chip_name = chipNameFromRaDecLSST(star_ra, star_dec,
+                                          obs_metadata=obs_md).astype(str)
+        valid = np.char.find(chip_name, 'None')<0
+
+        obs_mask[:,i_obs] = valid
 
 if __name__ == "__main__":
 
@@ -110,13 +146,30 @@ if __name__ == "__main__":
     kplr_dummy = variability.ParametrizedLightCurveMixin()
     kplr_dummy.load_parametrized_light_curves()
 
+    lookup_name = os.path.join(cache_dir, 'htmid_6_to_obsHistID_lookup.h5')
+    assert os.path.isfile(lookup_name)
+
+    htmid_to_ct = {}
+    with h5py.File(lookup_name, 'r') as in_file:
+        for k in in_file.keys():
+            try:
+                htmid = int(k)
+            except ValueError:
+                continue
+
+            n_obs = len(in_file['%d' % htmid][()])
+            htmid_to_ct[htmid] = n_obs
+
     stellar_db_name = os.path.join(cache_dir, 'dc2_stellar_db.db')
     assert os.path.isfile(stellar_db_name)
 
+    htmid = 8978
+    chunk_size = 50000000//htmid_to_ct[htmid]
+    print('chunk_size %d' % chunk_size)
 
     query = "SELECT simobjid, htmid_6, sedFilename, magNorm, ebv, "
     query += "varParamStr, parallax, ra, decl FROM stars "
-    query += "WHERE htmid_6=8978"
+    query += "WHERE htmid_6=%d" % htmid
 
     mgr = multiprocessing.Manager()
     my_lock = mgr.Lock()
@@ -125,7 +178,7 @@ if __name__ == "__main__":
     with sqlite3.connect(stellar_db_name) as conn:
         cursor = conn.cursor()
         data_iterator = cursor.execute(query)
-        chunk = data_iterator.fetchmany(10000)
+        chunk = data_iterator.fetchmany(chunk_size)
         t_start = time.time()
         do_photometry(chunk, my_lock, output_dict)
         print('that took %e seconds' % (time.time()-t_start))
