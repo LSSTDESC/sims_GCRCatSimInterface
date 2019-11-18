727cf42e038e4981ab6c7a9e10dfdd343b9a82e4
Jim Chiang <jchiang@slac.stanford.edu>
2019-11-18 15:09:03 -0800

    enable configuration of various runtime options via .ini file

--- scripts/extract_truth_info.py
+++ scripts/extract_truth_info.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import sys
 from collections import defaultdict
 import multiprocessing
 import numpy as np
@@ -7,10 +8,22 @@ import pandas as pd
 from lsst.sims.photUtils import BandpassDict
 import desc.imsim
 from desc.imsim.imSim import find_file_path
+from get_config import get_config
 
 bp_dict = BandpassDict.loadTotalBandpassesFromFiles()
 
-def extract_sed(object_line, sed_dirs, gamma2_sign=-1):
+try:
+    config_file = sys.argv[1]
+except IndexError:
+    config_file = 'instcat_validation_config.ini'
+config = get_config(config_file)
+
+def get_image_dirs():
+    """Dummy function needed for FitsImage entries"""
+    pass
+
+def extract_sed(object_line, sed_dirs, gamma2_sign=-1,
+                disable_MW=False):
     params = object_line.strip().split()
     unique_id = params[1]
     ra_phosim = float(params[2])
@@ -85,31 +98,29 @@ def extract_sed(object_line, sed_dirs, gamma2_sign=-1):
             galactic_av = float(params[i_gal_dust_model+1])
             galactic_rv = float(params[i_gal_dust_model+2])
 
-
     mu = 1./((1. - kappa)**2 - (gamma1**2 + gamma2**2))
     sed_file = find_file_path(sed_name, sed_dirs)
-    return unique_id, ra_phosim, dec_phosim, mu, \
+    if disable_MW:
+        galactic_av = 0
+    return unique_id, ra_phosim, dec_phosim, mu, gs_type, \
         desc.imsim.SedWrapper(sed_file, mag_norm, redshift,
                               internal_av, internal_rv,
                               galactic_av, galactic_rv, bp_dict)
 
 
-def photParams(instcat):
-    return desc.imsim.photometricParameters(
-        desc.imsim.metadata_from_file(instcat))
-
-
 def extract_truth_info(instcat, sed_dirs):
+    global config
     obj_ids = []
     gal_ids = []
     ras, decs = [], []
+    gs_types = []
     fluxes = defaultdict(list)
-    phot_params = photParams(instcat)
     with open(instcat) as fd:
         for line in fd:
             if not line.startswith('object'):
                 continue
-            obj_id, ra, dec, mu, sed = extract_sed(line, sed_dirs)
+            obj_id, ra, dec, mu, gs_type, sed \
+                = extract_sed(line, sed_dirs, disable_MW=config['disable_MW'])
             try:
                 gal_id = int(obj_id) >> 10
             except ValueError:
@@ -118,16 +129,22 @@ def extract_truth_info(instcat, sed_dirs):
             gal_ids.append(gal_id)
             ras.append(ra)
             decs.append(dec)
+            gs_types.append(gs_type)
             for band in 'ugrizy':
                 fluxes[band].append(mu*sed.sed_obj.calcFlux(bp_dict[band]))
 
-    data = {'obj_id': obj_ids, 'gal_id': gal_ids, 'ra': ras, 'dec': decs}
+    data = {'obj_id': obj_ids, 'gal_id': gal_ids, 'ra': ras, 'dec': decs,
+            'gs_type': gs_types}
     for band in 'ugrizy':
         data[f'flux_{band}'] = fluxes[band]
     return pd.DataFrame(data=data)
 
 
-def make_gal_truthcat(df):
+def make_gal_truthcat(df_arg, exclude_gals_w_knots=False):
+    if exclude_gals_w_knots:
+        df = df_arg.query('gs_type != "RandomWalk"')
+    else:
+        df = df_arg
     gal_ids = set(df['gal_id'])
     fluxes = defaultdict(lambda: np.zeros(len('ugrizy')))
     coords = defaultdict(lambda: np.zeros(2))
@@ -147,10 +164,16 @@ def make_gal_truthcat(df):
     return pd.DataFrame(data=data)
 
 
-def make_truth_cats(det_name, sed_dirs):
-    instcat = f'instcats/{det_name}_instcat.txt'
-    truthcat = f'my_truth_cats/{det_name}_truthcat.pickle'
-    gal_truthcat = f'my_truth_cats/{det_name}_gal_truthcat.pickle'
+def make_truth_cats(det_name, sed_dirs, visit_dir):
+    global config
+    instcat_dir = f'instcats_{visit_dir}'
+    os.makedirs(instcat_dir, exist_ok=True)
+    my_truthcat_dir = config['ic_truthcat_prefix'] + f'_{visit_dir}'
+    os.makedirs(my_truthcat_dir, exist_ok=True)
+
+    instcat = f'{instcat_dir}/{det_name}_instcat.txt'
+    truthcat = f'{my_truthcat_dir}/{det_name}_truthcat.pickle'
+    gal_truthcat = f'{my_truthcat_dir}/{det_name}_gal_truthcat.pickle'
 
     if not os.path.isfile(truthcat):
         print(f'generating {det_name} truthcat')
@@ -161,7 +184,7 @@ def make_truth_cats(det_name, sed_dirs):
 
     if not os.path.isfile(gal_truthcat):
         print(f'generating {det_name} galaxy truthcat')
-        gal_df = make_gal_truthcat(df)
+        gal_df = make_gal_truthcat(df, exclude_gals_w_knots=False)
         gal_df.to_pickle(gal_truthcat)
     else:
         gal_df = pd.read_pickle(gal_truthcat)
@@ -169,22 +192,30 @@ def make_truth_cats(det_name, sed_dirs):
     return df, gal_df
 
 if __name__ == '__main__':
-    sed_dirs = (os.environ['SIMS_SED_LIBRARY_DIR'],
-                '/home/instcats/Run2.2i/00479028')
+    sed_dirs = [os.path.dirname(_) for _ in glob.glob('00*/phosim_cat*.txt')]
+    sed_dirs.append(os.environ['SIMS_SED_LIBRARY_DIR'])
 
-    det_names = []
-    with open('det_names.txt') as fd:
-        for line in fd:
-            det_name = 'R{}{}_S{}{}'.format(*[_ for _ in line if _.isdigit()])
-            if os.path.isfile(f'instcats/{det_name}_instcat.txt'):
-                det_names.append(det_name)
+#    visits = dict(u=2338, g=159521, r=40325, i=479028, z=8005, y=5883)
+    visits = dict(i=6824)
 
-    processes = 7
-    with multiprocessing.Pool(processes=processes) as pool:
-        workers = []
-        for det_name in det_names[2:]:
-            workers.append(pool.apply_async(make_truth_cats,
-                                            (det_name, sed_dirs)))
-        pool.close()
-        pool.join()
-        [_.get() for _ in workers]
+    processes = config['processes']
+    bands = config['bands']
+
+    for band in bands:
+        visit = visits[band]
+        visit_dir = f'{visit:08d}'
+        det_names = []
+        with open(f'sensor_list_{visit_dir}.txt', 'r') as fd:
+            for line in fd:
+                det_name = 'R{}{}_S{}{}'.format(*[_ for _ in line if _.isdigit()])
+                if os.path.isfile(f'instcats_{visit_dir}/{det_name}_instcat.txt'):
+                    det_names.append(det_name)
+        print(det_names)
+        with multiprocessing.Pool(processes=processes) as pool:
+            workers = []
+            for det_name in det_names:
+                workers.append(pool.apply_async(make_truth_cats,
+                                                (det_name, sed_dirs, visit_dir)))
+            pool.close()
+            pool.join()
+            [_.get() for _ in workers]
--- scripts/make_trimmed_instcats.py
+++ scripts/make_trimmed_instcats.py
@@ -1,4 +1,5 @@
 import os
+import glob
 from lsst.afw import cameraGeom
 from lsst.obs.lsstSim import LsstSimMapper
 from desc.imsim import InstCatTrimmer
@@ -17,16 +18,17 @@ def write_instcat(commands, obj_lines, outfile):
             output.write("{}\n".format(line.strip()))
 
 camera = LsstSimMapper().camera
-#sensor_list = [det.getName() for det in camera
-#               if det.getType() == cameraGeom.SCIENCE]
-sensor_list = ['R:2,2 S:1,1']
+sensor_list = [det.getName() for det in camera
+               if det.getType() == cameraGeom.SCIENCE]
 
-instcat = '/home/instcats/Run2.2i/00479028/phosim_cat_479028.txt'
-trimmer = InstCatTrimmer(instcat, sensor_list, log_level='DEBUG')
-
-for sensor_id in sensor_list:
-    outfile = trim_file_name(sensor_id, 'instcats')
-    if os.path.isfile(outfile):
-        continue
-    write_instcat(trimmer.command_lines, trimmer[sensor_id], outfile)
-
+instcats = sorted(glob.glob('00*/phosim_cat*.txt'))
+for instcat in instcats:
+    folder = os.path.dirname(instcat)
+    outdir = f'instcats_{folder}'
+    os.makedirs(outdir, exist_ok=True)
+    trimmer = InstCatTrimmer(instcat, sensor_list, log_level='DEBUG')
+    for sensor_id in sensor_list:
+        outfile = trim_file_name(sensor_id, outdir)
+        if os.path.isfile(outfile):
+            continue
+        write_instcat(trimmer.command_lines, trimmer[sensor_id], outfile)
