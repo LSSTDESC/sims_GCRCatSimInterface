1a31a5480cc7670052cc2578f63383f0e5ac617b
Jim Chiang <jchiang@slac.stanford.edu>
2020-02-11 20:18:25 -0800

    update travis-ci build to use lsstdesc/stack-sims docker image

---
+++ travis_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source scl_source enable devtoolset-8
+source loadLSST.bash
+setup -t sims_w_2019_42 lsst_sims
+setup -t DC2production throughputs
+setup -t DC2production sims_skybrightness_data
+pip install nose
+pip install pylint
+eups declare sims_GCRCatSimInterface -r ${TRAVIS_BUILD_DIR} -t current
+setup sims_GCRCatSimInterface
+cd ${TRAVIS_BUILD_DIR}
+scons
+nosetests -s --with-coverage --cover-package=desc.sims.GCRCatSimInterface
