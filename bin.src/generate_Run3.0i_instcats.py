f55d10ef5dd5535b4a59a6ab02567f32a8ed45f2
Jim Chiang <jchiang@slac.stanford.edu>
2020-05-18 09:20:17 -0700

    multiprocessing performs much better if all instcat generation scripts for a given visit are run in a single process

--- bin.src/generate_Run3.0i_instcats.py
+++ bin.src/generate_Run3.0i_instcats.py
@@ -35,53 +35,50 @@ class GenerateInstcat:
             subprocess.check_call(command, shell=True)
 
     def __call__(self, visit, dry_run=False):
+        commands = []
         # Generate instcat for un-sprinkled objects.
-        command = (f'time generateInstCat.py --config_file {self.config_file} '
-                   f'--fov {self.fov} --ids {visit} --agn_threads 1 '
-                   f'--out_dir {self.instcat_dir}')
-        self._subprocess_check_call(command, dry_run)
+        commands.append('time generateInstCat.py '
+                        f'--config_file {self.config_file} '
+                        f'--fov {self.fov} --ids {visit} --agn_threads 1 '
+                        f'--out_dir {self.instcat_dir}')
 
         # Instcats for sprinkled objects:
         visit_dir = f'{visit:08d}'
+        phosim_cat_file = os.path.join(self.instcat_dir, visit_dir,
+                                       f'phosim_cat_{visit}.txt')
 
         # Strongly lensed AGNs
         agn_truth_cat = os.path.join(self.config['truth_table_dir'],
                                      self.config['agn_truth_cat_basename'])
         file_out = os.path.join(self.instcat_dir, visit_dir,
                                 f'lensed_agn_{visit}.txt')
-        command = (f'time python {self.script_dir}/create_agn_ic.py '
-                   f'--obs_db {self.opsim_db_file} '
-                   f'--obs_id {visit} '
-                   f'--agn_truth_cat {agn_truth_cat} '
-                   f'--file_out {file_out}')
-        self._subprocess_check_call(command, dry_run)
-        command = f'gzip -f {file_out}'
-        self._subprocess_check_call(command, dry_run)
-        if not dry_run:
-            phosim_cat_file = os.path.join(self.instcat_dir, visit_dir,
-                                           f'phosim_cat_{visit}.txt')
-            with open(phosim_cat_file, 'a') as output:
-                output.write('.'.join((f'includeobj {file_out}', 'gz\n')))
+        commands.append(f'time python {self.script_dir}/create_agn_ic.py '
+                        f'--obs_db {self.opsim_db_file} '
+                        f'--obs_id {visit} '
+                        f'--agn_truth_cat {agn_truth_cat} '
+                        f'--file_out {file_out}')
+        commands.append(f'gzip -f {file_out}')
+        commands.append(f'echo "includeobj {os.path.basename(file_out)}.gz" '
+                        f'>> {phosim_cat_file}')
+        commands.append('echo')
 
         # Strongly lensed SNe
         sne_truth_cat = os.path.join(self.config['truth_table_dir'],
                                      self.config['sne_truth_cat_basename'])
         output_dir = os.path.join(self.instcat_dir, visit_dir)
         cat_file_name = f'lensed_sne_{visit}.txt'
-        command = (f'time python {self.script_dir}/create_sne_ic.py '
-                   f'--obs_db {self.opsim_db_file} '
-                   f'--obs_id {visit} '
-                   f'--sne_truth_cat {sne_truth_cat} '
-                   f'--output_dir {output_dir} '
-                   '--sed_folder Dynamic '
-                   f'--cat_file_name {cat_file_name} ')
-        self._subprocess_check_call(command, dry_run)
         file_out = os.path.join(output_dir, cat_file_name)
-        command = f'gzip -f {file_out}'
-        self._subprocess_check_call(command, dry_run)
-        if not dry_run:
-            with open(phosim_cat_file, 'a') as output:
-                output.write('.'.join((f'includeobj {file_out}', 'gz\n')))
+        commands.append(f'time python {self.script_dir}/create_sne_ic.py '
+                        f'--obs_db {self.opsim_db_file} '
+                        f'--obs_id {visit} '
+                        f'--sne_truth_cat {sne_truth_cat} '
+                        f'--output_dir {output_dir} '
+                        '--sed_folder Dynamic '
+                        f'--cat_file_name {cat_file_name}')
+        commands.append(f'gzip -f {file_out}')
+        commands.append(f'echo "includeobj {os.path.basename(file_out)}.gz" '
+                        f'>> {phosim_cat_file}')
+        commands.append('echo')
 
         # Lensed host galaxies
         host_truth_cat = os.path.join(self.config['truth_table_dir'],
@@ -89,20 +86,20 @@ class GenerateInstcat:
         fits_stamp_dir = self.config['fits_stamp_dir']
         file_out = os.path.join(self.instcat_dir, visit_dir,
                                 f'lensed_hosts_{visit}.txt')
-        command = (f'time python {self.script_dir}/create_lensed_host_ic.py '
-                   f'--obs_db {self.opsim_db_file} '
-                   f'--obs_id {visit} '
-                   f'--fov {self.fov} '
-                   f'--host_truth_cat {host_truth_cat} '
-                   f'--fits_stamp_dir {fits_stamp_dir} '
-                   f'--file_out {file_out} ')
-        self._subprocess_check_call(command, dry_run)
-        command = f'gzip -f {file_out}'
-        self._subprocess_check_call(command, dry_run)
-        if not dry_run:
-            with open(phosim_cat_file, 'a') as output:
-                output.write('.'.join((f'includeobj {file_out}', 'gz\n')))
+        commands.append('time python '
+                        f'{self.script_dir}/create_lensed_host_ic.py '
+                        f'--obs_db {self.opsim_db_file} '
+                        f'--obs_id {visit} '
+                        f'--fov {self.fov} '
+                        f'--host_truth_cat {host_truth_cat} '
+                        f'--fits_stamp_dir {fits_stamp_dir} '
+                        f'--file_out {file_out}')
+        commands.append(f'gzip -f {file_out}')
+        commands.append(f'echo "includeobj {os.path.basename(file_out)}.gz" '
+                        f'>> {phosim_cat_file}')
 
+        command = ';\n'.join(commands)
+        self._subprocess_check_call(command, dry_run)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -129,7 +126,7 @@ if __name__ == '__main__':
         end = len(visits) if args.end is None else args.end
         visits = visits[args.start: end]
 
-    processes = args.processes if len(visits) > 1 else 1
+    processes = min(args.processes, len(visits))
     gen_instcat = GenerateInstcat(args.config_file)
     if processes == 1:
         for visit in visits:
